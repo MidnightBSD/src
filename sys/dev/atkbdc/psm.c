9704aa7d152238b4f1612a47691475d156248c4a
Lucas Holt <luke@foolishgames.com>
2006-08-24 05:37:56 +0000

    Added psm ID's for glide point devices commonly found on laptops. Brings PSM support to FreeBSD 6.1 release.

--- sys/dev/atkbdc/psm.c
+++ sys/dev/atkbdc/psm.c
@@ -59,7 +59,7 @@
  */
 
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: src/sys/dev/atkbdc/psm.c,v 1.86.2.1 2006/01/11 20:27:23 dumbbell Exp $");
+__FBSDID("$FreeBSD: /repoman/r/ncvs/src/sys/dev/atkbdc/psm.c,v 1.86.2.1.2.1 2006/04/30 05:32:35 kensmith Exp $");
 
 #include "opt_isa.h"
 #include "opt_psm.h"
@@ -3565,9 +3565,12 @@ static driver_t psmcpnp_driver = {
 
 static struct isa_pnp_id psmcpnp_ids[] = {
 	{ 0x030fd041, "PS/2 mouse port" },		/* PNP0F03 */
+	{ 0x0e0fd041, "PS/2 mouse port" },		/* PNP0F0E */
+	{ 0x120fd041, "PS/2 mouse port" },		/* PNP0F12 */
 	{ 0x130fd041, "PS/2 mouse port" },		/* PNP0F13 */
 	{ 0x1303d041, "PS/2 port" },			/* PNP0313, XXX */
 	{ 0x02002e4f, "Dell PS/2 mouse port" },		/* Lat. X200, Dell */
+	{ 0x0002a906, "ALPS Glide Point" },		/* ALPS Glide Point */
 	{ 0x80374d24, "IBM PS/2 mouse port" },		/* IBM3780, ThinkPad */
 	{ 0x81374d24, "IBM PS/2 mouse port" },		/* IBM3781, ThinkPad */
 	{ 0x0190d94d, "SONY VAIO PS/2 mouse port"},     /* SNY9001, Vaio */
