38fc434bff6dc7846829d67e0c9a6d8b59493da9
Lucas Holt <luke@foolishgames.com>
2006-08-27 23:41:08 +0000

    Mergemaster now checks $MidnightBSD$

--- usr.sbin/mergemaster/mergemaster.sh
+++ usr.sbin/mergemaster/mergemaster.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
 # mergemaster
-
+#
 # Compare files created by /usr/src/etc/Makefile (or the directory
 # the user specifies) with the currently installed copies.
-
+#
 # Copyright 1998-2004 Douglas Barton
 # DougB@FreeBSD.org
-
+#
 # $FreeBSD: src/usr.sbin/mergemaster/mergemaster.sh,v 1.51.8.2 2006/02/13 23:47:48 rwatson Exp $
+# $MidnightBSD$
 
 PATH=/bin:/usr/bin:/usr/sbin
 
 display_usage () {
-  VERSION_NUMBER=`grep "[$]FreeBSD:" $0 | cut -d ' ' -f 4`
+  VERSION_NUMBER=`grep "[$]MidnightBSD:" $0 | cut -d ' ' -f 4`
   echo "mergemaster version ${VERSION_NUMBER}"
   echo 'Usage: mergemaster [-scrvahipCP] [-m /path]'
   echo '         [-t /path] [-d] [-u N] [-w N] [-D /path]'
@@ -408,7 +409,7 @@ fi
 
 # Define what CVS $Id tag to look for to aid portability.
 #
-CVS_ID_TAG=FreeBSD
+CVS_ID_TAG=MidnightBSD
 
 delete_temproot () {
   rm -rf "${TEMPROOT}" 2>/dev/null
