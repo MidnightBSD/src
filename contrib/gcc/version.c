112379bcc42a84e425ee8115f71711f43dd6ce51
D. Adam Karim <archite@midnightbsd.org>
2006-09-14 03:56:55 +0000

    Changed gcc version to indicate MidnightBSD

--- contrib/gcc/version.c
+++ contrib/gcc/version.c
@@ -1,3 +1,4 @@
+/* $MidnightBSD$ */
 /* $FreeBSD: src/contrib/gcc/version.c,v 1.12 2005/06/03 03:52:25 kan Exp $ */
 #include "version.h"
 
@@ -6,7 +7,7 @@
    please modify this string to indicate that, e.g. by putting your
    organization's name in parentheses at the end of the string.  */
 
-const char version_string[] = "3.4.4 [FreeBSD] 20050518";
+const char version_string[] = "3.4.4 [MidnightBSD] 20050518";
 
 /* This is the location of the online document giving instructions for
    reporting bugs.  If you distribute a modified version of GCC,
