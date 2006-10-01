6f07740baba9513e15e905d6d2702a52247884de
Lucas Holt <luke@foolishgames.com>
2006-10-01 21:08:37 +0000

    fix ports tarball name

--- release/scripts/ports-install.sh
+++ release/scripts/ports-install.sh
@@ -8,5 +8,5 @@ if [ "`id -u`" != "0" ]; then
 	exit 1
 fi
 echo "Extracting ports tarball into ${DESTDIR}/usr"
-cat ports.tgz | tar --unlink -xpzf - -C ${DESTDIR}/usr
+cat mports.tgz | tar --unlink -xpzf - -C ${DESTDIR}/usr
 exit 0
