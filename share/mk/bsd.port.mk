7484cb34666565fd0ab71c6ec9998ca32e61aa62
Lucas Holt <luke@foolishgames.com>
2006-10-01 15:18:11 +0000

    Convert to mports.

--- share/mk/bsd.port.mk
+++ share/mk/bsd.port.mk
@@ -1,7 +1,7 @@
 # $FreeBSD: src/share/mk/bsd.port.mk,v 1.307 2004/07/02 20:47:18 eik Exp $
-# $MidnightBSD$
+# $MidnightBSD: src/share/mk/bsd.port.mk,v 1.2 2006/05/22 06:03:21 laffer1 Exp $
 
-PORTSDIR?=	/usr/ports
+PORTSDIR?=	/usr/mports
 BSDPORTMK?=	${PORTSDIR}/Mk/bsd.port.mk
 
 .include <bsd.own.mk>
--- share/mk/bsd.port.subdir.mk
+++ share/mk/bsd.port.subdir.mk
@@ -1,7 +1,7 @@
 # $FreeBSD: src/share/mk/bsd.port.subdir.mk,v 1.31 2004/07/02 20:47:18 eik Exp $
-# $MidnightBSD$
+# $MidnightBSD: src/share/mk/bsd.port.subdir.mk,v 1.2 2006/05/22 06:03:21 laffer1 Exp $
 
-PORTSDIR?=	/usr/ports
+PORTSDIR?=	/usr/mports
 BSDPORTSUBDIRMK?=	${PORTSDIR}/Mk/bsd.port.subdir.mk
 
 .include "${BSDPORTSUBDIRMK}"
