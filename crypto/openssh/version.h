0a7da5dbe88e56fc2a1db03470d0c5c9e4b81d4a
Lucas Holt <luke@foolishgames.com>
2006-10-28 17:34:35 +0000

    $MidnightBSD$

--- crypto/openssh/version.h
+++ crypto/openssh/version.h
@@ -1,12 +1,13 @@
 /* $OpenBSD: version.h,v 1.47 2006/08/30 00:14:37 djm Exp $ */
 /* $FreeBSD: src/crypto/openssh/version.h,v 1.30.2.3 2006/10/06 14:07:17 des Exp $ */
+/* $MidnightBSD$ */
 
 #ifndef SSH_VERSION
 
 #define SSH_VERSION             (ssh_version_get())
 #define SSH_RELEASE             (ssh_version_get())
 #define SSH_VERSION_BASE        "OpenSSH_4.4p1"
-#define SSH_VERSION_ADDENDUM    "FreeBSD-20060930"
+#define SSH_VERSION_ADDENDUM    "MidnightBSD-20061010"
 
 const char *ssh_version_get(void);
 void ssh_version_set_addendum(const char *add);
