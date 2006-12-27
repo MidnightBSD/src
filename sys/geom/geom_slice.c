2e1ae7f0866ea6aec8fd53690a5984095fd4bd3d
Lucas Holt <luke@foolishgames.com>
2006-12-27 04:41:43 +0000

    Allow to use g_slice_orphan() from outside.

--- sys/geom/geom_slice.c
+++ sys/geom/geom_slice.c
@@ -52,7 +52,6 @@ __FBSDID("$FreeBSD: src/sys/geom/geom_slice.c,v 1.57.2.1 2005/10/08 22:11:38 rod
 #include <geom/geom_slice.h>
 #include <machine/stdarg.h>
 
-static g_orphan_t g_slice_orphan;
 static g_access_t g_slice_access;
 static g_start_t g_slice_start;
 
@@ -484,7 +483,7 @@ g_slice_new(struct g_class *mp, u_int slices, struct g_provider *pp, struct g_co
 	return (gp);
 }
 
-static void
+void
 g_slice_orphan(struct g_consumer *cp)
 {
 
