30c919425ecb5cf317c9ebd5452e28cc9786fcc4
Lucas Holt <luke@foolishgames.com>
2007-03-13 21:52:10 +0000

    Fix version

--- crypto/openssh/version.h
+++ crypto/openssh/version.h
@@ -1,6 +1,13 @@
-/* $OpenBSD: version.h,v 1.49 2007/03/06 10:13:14 djm Exp $ */
+/* $OpenBSD: version.h,v 1.47 2006/08/30 00:14:37 djm Exp $ */
+/* $MidnightBSD: src/crypto/openssh/version.h,v 1.6 2006/10/28 17:34:35 laffer1 Exp $ */
 
-#define SSH_VERSION	"OpenSSH_4.6"
+#ifndef SSH_VERSION
 
-#define SSH_PORTABLE	"p1"
-#define SSH_RELEASE	SSH_VERSION SSH_PORTABLE
+#define SSH_VERSION             (ssh_version_get())
+#define SSH_RELEASE             (ssh_version_get())
+#define SSH_VERSION_BASE        "OpenSSH_4.6p1"
+#define SSH_VERSION_ADDENDUM    "MidnightBSD-20070313"
+
+const char *ssh_version_get(void);
+void ssh_version_set_addendum(const char *add);
+#endif /* SSH_VERSION */
