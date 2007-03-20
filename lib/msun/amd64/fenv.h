65848cf3c085bcd400c254e61e7644c839e85d88
Lucas Holt <luke@foolishgames.com>
2007-03-20 17:02:55 +0000

    $MidnightBSD$
    The __MBSDID macro is not working as it should be with the assembler. There must be a behavior difference between FBSDID and MBSDID somewhere.

--- lib/msun/amd64/fenv.h
+++ lib/msun/amd64/fenv.h
@@ -23,6 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
+ * $MidnightBSD$
  * $FreeBSD: src/lib/msun/amd64/fenv.h,v 1.5 2005/03/16 22:34:14 das Exp $
  */
 
