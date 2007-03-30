4fb4debc75bb2271cedcaa5aa75e00362f0346ee
Chris Reinhardt <ctriv@midnightbsd.org>
2007-03-30 05:36:52 +0000

    Add a new variable, PORT_SYSTEM.  If you set PORT_SYSTEM to "mport" in your make.conf, your system will use the new mport package database and tools instead of the old pkg_* binaries and /var/db/pkg.
    Given that mport is in very early developement, and isn't even close to
    alpha code yet, you will be ill advised to use it on your production
    machine.

--- share/mk/bsd.port.mk
+++ share/mk/bsd.port.mk
@@ -1,8 +1,13 @@
 # $FreeBSD: src/share/mk/bsd.port.mk,v 1.307 2004/07/02 20:47:18 eik Exp $
-# $MidnightBSD: src/share/mk/bsd.port.mk,v 1.2 2006/05/22 06:03:21 laffer1 Exp $
+# $MidnightBSD: src/share/mk/bsd.port.mk,v 1.3 2006/10/01 15:18:11 laffer1 Exp $
 
 PORTSDIR?=	/usr/mports
+
+.if defined(PORT_SYSTEM) && ${PORT_SYSTEM:L} == mport
+BSDPORTMK?=     ${PORTSDIR}/Mk/bsd.mport.mk
+.else
 BSDPORTMK?=	${PORTSDIR}/Mk/bsd.port.mk
+.endif
 
 .include <bsd.own.mk>
 .include "${BSDPORTMK}"
