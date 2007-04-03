515d0b1874f272b9f9532cdf9e3943336f013b3b
Lucas Holt <luke@foolishgames.com>
2007-04-03 14:42:19 +0000

    Prevent a panic by checking for NULL and print a message to that effect.

--- sys/fs/fifofs/fifo_vnops.c
+++ sys/fs/fifofs/fifo_vnops.c
@@ -447,6 +447,11 @@ fifo_printinfo(vp)
 {
 	register struct fifoinfo *fip = vp->v_fifoinfo;
 
+	if (fip == NULL) {
+		printf(", NULL v_fifoinfo");
+		return (0);
+	}
+
 	printf(", fifo with %ld readers and %ld writers",
 		fip->fi_readers, fip->fi_writers);
 	return (0);
