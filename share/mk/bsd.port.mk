52bf19492f7fbd0bf519b39f6e9fddcca3d562e0
Chris Reinhardt <ctriv@midnightbsd.org>
2007-04-13 03:20:00 +0000

    bsd.mport.mk is now the default.  Let the instability in ports begin!

--- share/mk/bsd.port.mk
+++ share/mk/bsd.port.mk
@@ -2,12 +2,8 @@
 # $MidnightBSD: src/share/mk/bsd.port.mk,v 1.3 2006/10/01 15:18:11 laffer1 Exp $
 
 PORTSDIR?=	/usr/mports
+BSDPORTMK?=	${PORTSDIR}/Mk/bsd.mport.mk
 
-.if defined(PORT_SYSTEM) && ${PORT_SYSTEM:L} == mport
-BSDPORTMK?=     ${PORTSDIR}/Mk/bsd.mport.mk
-.else
-BSDPORTMK?=	${PORTSDIR}/Mk/bsd.port.mk
-.endif
 
 .include <bsd.own.mk>
 .include "${BSDPORTMK}"
