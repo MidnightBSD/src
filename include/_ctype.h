827a4148ba6a8298c34c5549bdbaaf153bc067f8
Lucas Holt <luke@foolishgames.com>
2007-04-13 21:16:04 +0000

    $MidnightBSD$

--- include/_ctype.h
+++ include/_ctype.h
@@ -41,6 +41,7 @@
  * From @(#)ctype.h	8.4 (Berkeley) 1/21/94
  * From FreeBSD: src/include/ctype.h,v 1.27 2004/06/23 07:11:39 tjr Exp
  * $FreeBSD: src/include/_ctype.h,v 1.30 2004/08/21 07:00:40 tjr Exp $
+ * $MidnightBSD$
  */
 
 #ifndef __CTYPE_H_
