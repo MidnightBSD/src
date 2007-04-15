95b2753ef9ebc343386c2cc1ce39ff383f69e4fc
Lucas Holt <luke@foolishgames.com>
2007-04-15 04:55:21 +0000

    whitespace

--- share/mk/bsd.port.mk
+++ share/mk/bsd.port.mk
@@ -1,9 +1,8 @@
 # $FreeBSD: src/share/mk/bsd.port.mk,v 1.307 2004/07/02 20:47:18 eik Exp $
-# $MidnightBSD: src/share/mk/bsd.port.mk,v 1.3 2006/10/01 15:18:11 laffer1 Exp $
+# $MidnightBSD: src/share/mk/bsd.port.mk,v 1.5 2007/04/13 03:20:00 ctriv Exp $
 
 PORTSDIR?=	/usr/mports
 BSDPORTMK?=	${PORTSDIR}/Mk/bsd.mport.mk
-
 
 .include <bsd.own.mk>
 .include "${BSDPORTMK}"
