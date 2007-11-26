6ee8dc0ec7453b8f0617841610d5ee4c6e41f281
Chris Reinhardt <ctriv@midnightbsd.org>
2007-11-26 21:43:15 +0000

    Add the mport.install libexec binary.

---
+++ libexec/mport.install/mport.install.c
@@ -0,0 +1,83 @@
+/*-
+ * Copyright (c) 2007 Chris Reinhardt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $MidnightBSD: src/libexec/mport.create/mport.create.c,v 1.3 2007/09/27 23:09:12 ctriv Exp $
+ */
+
+
+
+#include <sys/cdefs.h>
+__MBSDID("$MidnightBSD: src/libexec/mport.create/mport.create.c,v 1.3 2007/09/27 23:09:12 ctriv Exp $");
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <err.h>
+#include <string.h>
+#include <unistd.h>
+#include <mport.h>
+
+
+static void usage(void);
+
+int main(int argc, char *argv[]) 
+{
+  int ch, i;
+  char *prefix = NULL;
+    
+  while ((ch = getopt(argc, argv, "p:")) != -1) {
+    switch (ch) {
+      case 'p':
+        prefix = optarg;
+        break;
+      case '?':
+      default:
+        usage();
+        break; 
+    }
+  } 
+
+  argc -= optind;
+  argv += optind;
+  
+  for (i=0; i<argc; i++) {
+//    printf("filename: %s\n", argv[i]);
+    if (mport_install_pkg(argv[i], prefix) != MPORT_OK) {
+      warnx("%s", mport_err_string());
+      exit(1);
+    }
+  }
+  
+  return 0;
+}
+
+
+    
+
+static void usage() 
+{
+  fprintf(stderr, "Usage: mport.install [-p prefix] pkgfile1 pkgfile2 ...\n");
+  exit(2);
+}
