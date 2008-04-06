cc400b717a9cf2590e99e8c0269e85b14f9d21d3
Lucas Holt <luke@foolishgames.com>
2008-04-06 04:46:46 +0000

    Fix merge conflicts with the version.

--- crypto/openssh/version.h
+++ crypto/openssh/version.h
@@ -1,12 +1,10 @@
-/* $OpenBSD: version.h,v 1.47 2006/08/30 00:14:37 djm Exp $ */
-/* $MidnightBSD: src/crypto/openssh/version.h,v 1.6 2006/10/28 17:34:35 laffer1 Exp $ */
-
-#ifndef SSH_VERSION
+/* $MidnightBSD: src/crypto/openssh/version.h,v 1.8 2007/03/13 21:52:10 laffer1 Exp $ */
+/* $OpenBSD: version.h,v 1.52 2008/03/27 00:16:49 djm Exp $ */
 
 #define SSH_VERSION             (ssh_version_get())
 #define SSH_RELEASE             (ssh_version_get())
-#define SSH_VERSION_BASE        "OpenSSH_4.6p1"
-#define SSH_VERSION_ADDENDUM    "MidnightBSD-20070313"
+#define SSH_VERSION_BASE        "OpenSSH_4.9p1"
+#define SSH_VERSION_ADDENDUM    "MidnightBSD-20080406"
 
 const char *ssh_version_get(void);
 void ssh_version_set_addendum(const char *add);
