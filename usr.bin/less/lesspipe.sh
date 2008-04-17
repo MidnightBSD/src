e2b450951af772678a33c8f5aa0302eb5ac841a4
Lucas Holt <luke@foolishgames.com>
2008-04-17 23:42:30 +0000

    Update to v416.

--- usr.bin/less/lesspipe.sh
+++ usr.bin/less/lesspipe.sh
@@ -1,15 +1,17 @@
 #! /bin/sh
-# $FreeBSD: /repoman/r/ncvs/src/usr.bin/less/lesspipe.sh,v 1.2.2.1 2006/09/03 16:09:50 delphij Exp $
+# ex:ts=8
+#
+# $FreeBSD: src/usr.bin/less/lesspipe.sh,v 1.2.2.2 2007/05/27 10:15:18 le Exp $
 # $MidnightBSD$
 
 case "$1" in
 	*.Z)
-		exec uncompress -c $1	2>/dev/null
+		exec uncompress -c "$1"	2>/dev/null
 		;;
 	*.gz)
-		exec gzip -d -c $1	2>/dev/null
+		exec gzip -d -c "$1"	2>/dev/null
 		;;
 	*.bz2)
-		exec bzip2 -d -c $1	2>/dev/null
+		exec bzip2 -d -c "$1"	2>/dev/null
 		;;
 esac
