79773c36d787dff27ca48dbcfa5c05ebc77ae215
Lucas Holt <luke@foolishgames.com>
2008-04-18 00:20:44 +0000

    Update for 5.0p1

--- crypto/openssh/version.h
+++ crypto/openssh/version.h
@@ -1,12 +1,12 @@
-/* $MidnightBSD: src/crypto/openssh/version.h,v 1.9 2008/04/06 04:46:46 laffer1 Exp $ */
-/* $OpenBSD: version.h,v 1.52 2008/03/27 00:16:49 djm Exp $ */
+/* $MidnightBSD: src/crypto/openssh/version.h,v 1.10 2008/04/06 06:00:19 laffer1 Exp $ */
+/* $OpenBSD: version.h,v 1.53 2008/04/03 09:50:14 djm Exp $ */
 
 #ifndef SSH_VERSION
 
 #define SSH_VERSION             (ssh_version_get())
 #define SSH_RELEASE             (ssh_version_get())
-#define SSH_VERSION_BASE        "OpenSSH_4.9p1"
-#define SSH_VERSION_ADDENDUM    "MidnightBSD-20080406"
+#define SSH_VERSION_BASE        "OpenSSH_5.0p1"
+#define SSH_VERSION_ADDENDUM    "MidnightBSD-20080417"
 
 const char *ssh_version_get(void);
 void ssh_version_set_addendum(const char *add);
