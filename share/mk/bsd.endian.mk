a8d4a56f0390de67e9865a22f50e9bc7757a42bc
Lucas Holt <luke@foolishgames.com>
2008-04-28 05:53:25 +0000

    Remove powerpc, tweak arm since we haven't deciced if we're keeping it or not.

--- share/mk/bsd.endian.mk
+++ share/mk/bsd.endian.mk
@@ -1,13 +1,12 @@
-# $FreeBSD: src/share/mk/bsd.endian.mk,v 1.2 2005/02/25 00:24:03 cognet Exp $
-# $MidnightBSD: src/share/mk/bsd.endian.mk,v 1.2 2006/05/22 06:03:21 laffer1 Exp $
+# $MidnightBSD$
+# $FreeBSD: src/share/mk/bsd.endian.mk,v 1.4 2006/11/05 15:33:26 cognet Exp $
 
 .if ${MACHINE_ARCH} == "amd64" || \
     ${MACHINE_ARCH} == "i386" || \
     ${MACHINE_ARCH} == "ia64" || \
-    (${MACHINE_ARCH} == "arm" && !defined(ARM_BIG_ENDIAN))
+    (${MACHINE_ARCH} == "arm" && !defined(TARGET_BIG_ENDIAN))
 TARGET_ENDIANNESS= 1234
-.elif ${MACHINE_ARCH} == "powerpc" || \
-    ${MACHINE_ARCH} == "sparc64" || \
+.elif ${MACHINE_ARCH} == "sparc64" || \
     ${MACHINE_ARCH} == "arm"
 TARGET_ENDIANNESS= 4321
 .endif
