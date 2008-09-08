6c07c095737c501bf3db5061b8b9cf4d706e3a24
Lucas Holt <luke@foolishgames.com>
2008-09-08 01:41:18 +0000

    Remove unsupported architectures.

--- sys/arm/include/sigframe.h
+++
@@ -1,2 +0,0 @@
-/* $FreeBSD: src/sys/arm/include/sigframe.h,v 1.1 2004/05/14 11:46:44 cognet Exp $ */
-#include <machine/frame.h>
--- sys/arm/include/smp.h
+++
@@ -1,6 +0,0 @@
-/* $FreeBSD: src/sys/arm/include/smp.h,v 1.1 2004/05/14 11:46:44 cognet Exp $ */
-
-#ifndef _MACHINE_SMP_H_
-#define _MACHINE_SMP_H_
-
-#endif /* !_MACHINE_SMP_H_ */
