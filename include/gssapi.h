c49040e457cdd8cf67ae9665b0bdb02d3309da4a
Lucas Holt <luke@foolishgames.com>
2008-09-30 03:22:17 +0000

    add legacy include so that old code works.

---
+++ include/gssapi.h
@@ -0,0 +1,6 @@
+/* $MidnightBSD$ */
+/* $FreeBSD: src/include/gssapi.h,v 1.1 2005/12/29 14:40:19 dfr Exp $ */
+#ifdef __GNUC__
+#warning "this file includes <gssapi.h> which is deprecated, use <gssapi/gssapi.h> instead"
+#endif
+#include <gssapi/gssapi.h>
