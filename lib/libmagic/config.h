79cac3b952a9d4ac951c21100c318ea7abb5d995
Lucas Holt <luke@foolishgames.com>
2008-12-06 03:05:38 +0000

    stdint should always be set

--- lib/libmagic/config.h
+++ lib/libmagic/config.h
@@ -60,9 +60,7 @@
 #define HAVE_SNPRINTF 1
 
 /* Define to 1 if you have the <stdint.h> header file. */
-#if __FreeBSD_version >= 500019
 #define HAVE_STDINT_H 1
-#endif
 
 /* Define to 1 if you have the <stdlib.h> header file. */
 #define HAVE_STDLIB_H 1
