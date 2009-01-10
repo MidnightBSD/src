c23d029913d6af3fa25f6915c56edf0804c03e1c
Lucas Holt <luke@foolishgames.com>
2009-01-10 22:55:24 +0000

    *** empty log message ***

--- nrelease/install-iso2img.sh
+++ nrelease/install-iso2img.sh
@@ -5,7 +5,7 @@
 
 # Set serial variable to 0 if you don't want serial console at all,
 # 1 if you want comconsole and 2 if you want comconsole and vidconsole
-serial=3D0
+serial=0
 
 set -u
 
@@ -14,26 +14,23 @@ if [ $# -lt 2 ]; then
     exit 1
 fi
 
-isoimage=3D$1; shift
-imgoutfile=3D$1; shift
+isoimage=$1; shift
+imgoutfile=$1; shift
 
-export tmpdir=3D$(mktemp -d -t fbsdmount)
-# Temp file and directory to be used later
-export tmpfile=3D$(mktemp -t bsdmount)
-
-export isodev=3D$(mdconfig -a -t vnode -f ${isoimage})
+export tmpdir=$(mktemp -d -t fbsdmount)
+export isodev=$(mdconfig -a -t vnode -f ${isoimage})
 
 echo "#### Building bootable UFS image ####"
 
-ISOSIZE=3D$(du -k ${isoimage} | awk '{print $1}')
-SECTS=3D$((($ISOSIZE + ($ISOSIZE/5))*2))
+ISOSIZE=$(du -k ${isoimage} | awk '{print $1}')
+SECTS=$((($ISOSIZE + ($ISOSIZE/4))*4))
 
 # Root partition size
 
 echo "Initializing image..."
-dd if=3D/dev/zero of=3D${imgoutfile} count=3D${SECTS}
+dd if=/dev/zero of=${imgoutfile} count=${SECTS}
 ls -l ${imgoutfile}
-export imgdev=3D$(mdconfig -a -t vnode -f ${imgoutfile})
+export imgdev=$(mdconfig -a -t vnode -f ${imgoutfile})
 
 bsdlabel -w -B ${imgdev}
 newfs -O1 /dev/${imgdev}a
@@ -44,19 +41,18 @@ mount -t cd9660 /dev/${isodev} ${tmpdir}/iso
 mount /dev/${imgdev}a ${tmpdir}/img
 
 echo "Copying files to the image..."
-( cd ${tmpdir}/iso && find . -print -depth | cpio -dump ${tmpdir}/img )
+( cd ${tmpdir}/iso && find . -print -depth | gcpio -dump ${tmpdir}/img )
 #bzcat ${tmpdir}/iso/dist/root.dist.bz2 | mtree -PUr -p ${tmpdir}/img 2>&=
-1 > /dev/null
+#1 > /dev/null
 
 #echo "/dev/ufs/${UFS_LABEL} / ufs ro 1 1" > ${tmpdir}/img/etc/fstab
 
 if [ ${serial} -eq 2 ]; then
         echo "-D" > ${tmpdir}/img/boot.config
-        echo 'console=3D"comconsole, vidconsole"' >> ${tmpdir}/img/boot/l=
-oader.conf
+        echo 'console="comconsole, vidconsole"' >> ${tmpdir}/img/boot/loader.conf
 elif [ ${serial} -eq 1 ]; then
         echo "-h" > ${tmpdir}/img/boot.config
-        echo 'console=3D"comconsole"' >> ${tmpdir}/img/boot/loader.conf
+        echo 'console="comconsole"' >> ${tmpdir}/img/boot/loader.conf
 fi
 
 cleanup() {
@@ -64,7 +60,6 @@ cleanup() {
     mdconfig -d -u ${isodev}
     umount ${tmpdir}/img
     mdconfig -d -u ${imgdev}
-    rm -rf ${tmpdir} ${tmpfile}
 }
 
 cleanup
