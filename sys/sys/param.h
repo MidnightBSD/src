9356dfa03075f2f622b518c7a1632c755b2b22f1
Lucas Holt <luke@foolishgames.com>
2009-03-25 18:10:52 +0000

    bump version.  We've made a number of changes to both userland and the kernel.  Make it easier to sort this mess out.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -33,7 +33,7 @@
  *
  *	@(#)param.h	8.3 (Berkeley) 4/4/95
  * $FreeBSD: src/sys/sys/param.h,v 1.244.2.8 2006/01/18 13:24:24 andre Exp $
- * $MidnightBSD: src/sys/sys/param.h,v 1.15 2008/12/07 04:02:23 laffer1 Exp $
+ * $MidnightBSD: src/sys/sys/param.h,v 1.16 2008/12/09 16:35:01 laffer1 Exp $
  */
 
 #ifndef _SYS_PARAM_H_
@@ -61,7 +61,7 @@
 #define __FreeBSD_version 700055
 
 #undef __MidnightBSD_version
-#define __MidnightBSD_version 3000	/* Master, propagated to newvers */
+#define __MidnightBSD_version 3001	/* Master, propagated to newvers */
 
 #ifndef LOCORE
 #include <sys/types.h>
