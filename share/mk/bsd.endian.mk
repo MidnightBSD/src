62a6a53e120df2f00638f96d806e8f4a3637ab2a
Lucas Holt <luke@foolishgames.com>
2009-04-15 02:09:51 +0000

    we don't have that many architectures.

--- share/mk/bsd.endian.mk
+++ share/mk/bsd.endian.mk
@@ -1,12 +1,9 @@
-# $MidnightBSD$
+# $MidnightBSD: src/share/mk/bsd.endian.mk,v 1.4 2008/04/28 05:53:25 laffer1 Exp $
 # $FreeBSD: src/share/mk/bsd.endian.mk,v 1.4 2006/11/05 15:33:26 cognet Exp $
 
 .if ${MACHINE_ARCH} == "amd64" || \
-    ${MACHINE_ARCH} == "i386" || \
-    ${MACHINE_ARCH} == "ia64" || \
-    (${MACHINE_ARCH} == "arm" && !defined(TARGET_BIG_ENDIAN))
+    ${MACHINE_ARCH} == "i386"
 TARGET_ENDIANNESS= 1234
-.elif ${MACHINE_ARCH} == "sparc64" || \
-    ${MACHINE_ARCH} == "arm"
+.elif ${MACHINE_ARCH} == "sparc64"
 TARGET_ENDIANNESS= 4321
 .endif
