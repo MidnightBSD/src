0003243222125cfc0a4e0f43859036e91b40abab
Lucas Holt <luke@foolishgames.com>
2009-05-02 16:56:00 +0000

    version numbers can be fun.

--- crypto/openssh/version.h
+++ crypto/openssh/version.h
@@ -1,6 +1,13 @@
-/* $OpenBSD: version.h,v 1.55 2009/02/23 00:06:15 djm Exp $ */
+/* $MidnightBSD: src/crypto/openssh/version.h,v 1.11 2008/04/18 00:20:44 laffer1 Exp $ */
+/* $OpenBSD: version.h,v 1.53 2008/04/03 09:50:14 djm Exp $ */
 
-#define SSH_VERSION	"OpenSSH_5.2"
+#ifndef SSH_VERSION
 
-#define SSH_PORTABLE	"p1"
-#define SSH_RELEASE	SSH_VERSION SSH_PORTABLE
+#define SSH_VERSION             (ssh_version_get())
+#define SSH_RELEASE             (ssh_version_get())
+#define SSH_VERSION_BASE        "OpenSSH_5.2p1"
+#define SSH_VERSION_ADDENDUM    "MidnightBSD-20090502"
+
+const char *ssh_version_get(void);
+void ssh_version_set_addendum(const char *add);
+#endif /* SSH_VERSION */
