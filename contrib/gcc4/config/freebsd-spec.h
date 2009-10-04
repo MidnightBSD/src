24ae202260c0700b064f3e5a4a5b730d615cd78d
Lucas Holt <luke@foolishgames.com>
2009-10-04 15:06:50 +0000

    we're going to need to set this to 1 for it to make sense even though our major version number isn't there yet.

--- contrib/gcc4/config/freebsd-spec.h
+++ contrib/gcc4/config/freebsd-spec.h
@@ -53,7 +53,7 @@ Boston, MA 02110-1301, USA.  */
 #define FBSD_TARGET_OS_CPP_BUILTINS()					\
   do									\
     {									\
-	builtin_define_with_int_value ("__MidnightBSD__", 0);		\
+	builtin_define_with_int_value ("__MidnightBSD__", 1);		\
 	builtin_define_with_int_value ("__FreeBSD__", FBSD_MAJOR);	\
 	builtin_define_std ("unix");					\
 	builtin_define ("__KPRINTF_ATTRIBUTE__");		       	\
