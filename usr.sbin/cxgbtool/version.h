7c2ce2f7551287b9d38d00a384bed4c42ec88328
Lucas Holt <luke@foolishgames.com>
2009-10-07 02:42:43 +0000

    add cxgbtool for managing 10GB cards using cxgb

---
+++ usr.sbin/cxgbtool/version.h
@@ -0,0 +1,32 @@
+/*****************************************************************************
+ *                                                                           *
+ * File:                                                                     *
+ *  version.h                                                                *
+ *                                                                           *
+ * Description:                                                              *
+ *  cxgbtool userspace utility version defines.                              *
+ *                                                                           *
+ * http://www.chelsio.com                                                    *
+ *                                                                           *
+ * Copyright (c) 2003 - 2007 Chelsio Communications, Inc.                    *
+ * All rights reserved.                                                      *
+ *                                                                           *
+ * Maintainers: maintainers@chelsio.com                                      *
+ *                                                                           *
+ * History:                                                                  *
+ *                                                                           *
+ ****************************************************************************/
+/* $Date: 2009-10-07 02:42:43 $ $RCSfile: version.h,v $ $Revision: 1.1 $ */
+
+/*
+ * $FreeBSD: src/usr.sbin/cxgbtool/version.h,v 1.1 2007/03/14 02:51:57 kmacy Exp $
+ */
+
+#ifndef __CXGBTOOL_VERSION_H
+#define __CXGBTOOL_VERSION_H
+
+#define PROGNAME "cxgbtool"
+#define VERSION "1.8"
+#define COPYRIGHT "Copyright (c) 2004-2007 Chelsio Communications"
+
+#endif //__CXGBTOOL_VERSION_H
