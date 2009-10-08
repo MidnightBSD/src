a93b5aeadba62b1766b41fe8f4d365a460ebfd66
Lucas Holt <luke@foolishgames.com>
2009-10-08 01:27:57 +0000

    Bump version number so we can track all the userland (and timezone) changes in ports, etc.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -33,7 +33,7 @@
  *
  *	@(#)param.h	8.3 (Berkeley) 4/4/95
  * $FreeBSD: src/sys/sys/param.h,v 1.244.2.8 2006/01/18 13:24:24 andre Exp $
- * $MidnightBSD: src/sys/sys/param.h,v 1.20 2009/05/24 17:02:36 laffer1 Exp $
+ * $MidnightBSD: src/sys/sys/param.h,v 1.21 2009/06/10 15:35:50 laffer1 Exp $
  */
 
 #ifndef _SYS_PARAM_H_
@@ -61,7 +61,7 @@
 #define __FreeBSD_version 700055
 
 #undef __MidnightBSD_version
-#define __MidnightBSD_version 3005	/* Master, propagated to newvers */
+#define __MidnightBSD_version 3006	/* Master, propagated to newvers */
 
 #ifndef LOCORE
 #include <sys/types.h>
