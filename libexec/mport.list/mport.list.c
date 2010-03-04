5138045091b53742d859b6e7d59781beb6ed6be5
Lucas Holt <luke@foolishgames.com>
2010-03-04 01:09:58 +0000

    list all currently installed mports.

---
+++ libexec/mport.list/mport.list.c
@@ -0,0 +1,111 @@
+/*-
+ * Copyright (c) 2010 Lucas Holt
+ * Copyright (c) 2008 Chris Reinhardt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $MidnightBSD: src/libexec/mport.query/mport.query.c,v 1.3 2009/06/05 00:08:09 laffer1 Exp $
+ */
+
+
+
+#include <sys/cdefs.h>
+__MBSDID("$MidnightBSD: src/libexec/mport.query/mport.query.c,v 1.3 2009/06/05 00:08:09 laffer1 Exp $");
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <err.h>
+#include <string.h>
+#include <unistd.h>
+#include <mport.h>
+
+static void usage(void);
+
+int main(int argc, char *argv[]) 
+{
+  int ch;
+  mportInstance *mport;
+  mportPackageMeta **packs;
+  bool quiet = false;
+  bool version = false;
+
+  if (argc > 2)
+    usage();
+    
+  while ((ch = getopt(argc, argv, "qv")) != -1) {
+    switch (ch) {
+      case 'q':
+        quiet = true;
+        break;
+      case 'v':
+        version = true;
+        break;
+      case '?':
+      default:
+        usage();
+        break; 
+    }
+  } 
+
+  argc -= optind;
+  argv += optind;
+
+  mport = mport_instance_new();
+  
+  if (mport_instance_init(mport, NULL) != MPORT_OK) {
+    warnx("%s", mport_err_string());
+    exit(1);
+  }
+
+  if (mport_pkgmeta_list(mport, &packs) != MPORT_OK) {
+    warnx("%s", mport_err_string());
+    exit(1);
+  }
+  
+  if (packs == NULL) {
+    if (!quiet)
+      warnx("No packages installed matching.");
+    exit(3);
+  }
+  
+  while (*packs != NULL) {
+    if (version)
+      (void) printf("%s\t%s\n", (*packs)->name, (*packs)->version);
+    else
+      (void) printf("%s\n", (*packs)->name);
+    packs++;
+  }
+
+  mport_instance_free(mport); 
+  
+  return 0;
+}
+
+
+static void usage() 
+{
+  fprintf(stderr, "Usage: mport.list [-q | -v]\n");
+  exit(2);
+}
