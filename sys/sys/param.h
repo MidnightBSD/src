82dbfb44112a62eb85d08905581dcc40a170e7b1
Lucas Holt <luke@foolishgames.com>
2010-07-13 23:21:45 +0000

    Bump for sendfile(2) mbuf fix.

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -33,7 +33,7 @@
  *
  *	@(#)param.h	8.3 (Berkeley) 4/4/95
  * $FreeBSD: src/sys/sys/param.h,v 1.244.2.8 2006/01/18 13:24:24 andre Exp $
- * $MidnightBSD: src/sys/sys/param.h,v 1.27 2010/01/10 22:14:40 laffer1 Exp $
+ * $MidnightBSD: src/sys/sys/param.h,v 1.28 2010/03/18 00:12:58 laffer1 Exp $
  */
 
 #ifndef _SYS_PARAM_H_
@@ -61,7 +61,7 @@
 #define __FreeBSD_version 700055
 
 #undef __MidnightBSD_version
-#define __MidnightBSD_version 3012	/* Master, propagated to newvers */
+#define __MidnightBSD_version 3013	/* Master, propagated to newvers */
 
 #ifndef LOCORE
 #include <sys/types.h>
