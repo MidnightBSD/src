605b1fb9abdb87b12ec1f43240ebc3f908857fc9
Lucas Holt <luke@foolishgames.com>
2010-08-22 20:56:03 +0000

    MidnightBSD patches.

--- contrib/mDNSResponder/mDNSShared/CommonServices.h
+++ contrib/mDNSResponder/mDNSShared/CommonServices.h
@@ -92,6 +92,15 @@ Common Services and portability support for various platforms.
 	#endif
 #endif
 
+// FreeBSD
+
+#if( !defined( TARGET_OS_FREEBSD ) )
+	#if( defined( __FreeBSD__ ) )
+		#define	TARGET_OS_FREEBSD		1
+	#else
+		#define	TARGET_OS_FREEBSD		0
+	#endif
+#endif
 // Linux
 
 #if( !defined( TARGET_OS_LINUX ) )
@@ -128,7 +137,7 @@ Common Services and portability support for various platforms.
 	
 	// No predefined macro for VxWorks so just assume VxWorks if nothing else is set.
 	
-	#if( !macintosh && !__MACH__  && !defined( __linux__ ) && !defined ( __SVR4 ) && !defined ( __sun ) && !defined( __PALMOS_TRAPS__ ) && !defined( __PALMOS_ARMLET__ ) && !defined( _WIN32 ) )
+	#if( !macintosh && !__MACH__  && !defined( __FreeBSD__ ) && !defined( __linux__ ) && !defined ( __SVR4 ) && !defined ( __sun ) && !defined( __PALMOS_TRAPS__ ) && !defined( __PALMOS_ARMLET__ ) && !defined( _WIN32 ) )
 		#define	TARGET_OS_VXWORKS		1
 	#else
 		#define	TARGET_OS_VXWORKS		0
@@ -217,6 +226,15 @@ Common Services and portability support for various platforms.
 	#include	<libkern/OSTypes.h>
 	#include	<sys/types.h>
 	
+#elif( TARGET_OS_FREEBSD )
+
+	// FreeBSD
+	#include	<stdint.h>
+	#include	<pthread.h>
+	#include	<netinet/in.h>
+	#include	<arpa/inet.h>
+	#include	<sys/socket.h>
+
 #elif( TARGET_OS_LINUX )
 	
 	// Linux
@@ -493,7 +511,7 @@ Common Services and portability support for various platforms.
 // - Windows
 
 #if( TARGET_LANGUAGE_C_LIKE )
-	#if( !defined(_SSIZE_T) && ( TARGET_OS_WIN32 || !defined( _BSD_SSIZE_T_DEFINED_ ) ) && !TARGET_OS_LINUX && !TARGET_OS_VXWORKS && !TARGET_OS_MAC)
+	#if( !defined(_SSIZE_T) && ( TARGET_OS_WIN32 || !defined( _BSD_SSIZE_T_DEFINED_ ) ) && !TARGET_OS_FREEBSD && !TARGET_OS_LINUX && !TARGET_OS_VXWORKS && !TARGET_OS_MAC)
 		typedef int						ssize_t;
 	#endif
 #endif
