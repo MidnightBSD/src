23dab0db6b1fbd657b7a40c4626acdc1ff56dfc4
Lucas Holt <luke@foolishgames.com>
2010-12-11 05:49:35 +0000

    ansify and use null term character instead of just 0

--- lib/libc/string/strstr.c
+++ lib/libc/string/strstr.c
@@ -42,17 +42,16 @@ __FBSDID("$FreeBSD: src/lib/libc/string/strstr.c,v 1.5 2007/01/09 00:28:12 imp E
  * Find the first occurrence of find in s.
  */
 char *
-strstr(s, find)
-	const char *s, *find;
+strstr(const char *s, const char *find)
 {
 	char c, sc;
 	size_t len;
 
-	if ((c = *find++) != 0) {
+	if ((c = *find++) != '\0') {
 		len = strlen(find);
 		do {
 			do {
-				if ((sc = *s++) == 0)
+				if ((sc = *s++) == '\0')
 					return (NULL);
 			} while (sc != c);
 		} while (strncmp(s, find, len) != 0);
