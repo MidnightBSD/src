e168def1c1046fcbd2906929f1ac719da1bf4bcf
Lucas Holt <luke@foolishgames.com>
2011-01-29 23:00:34 +0000

    fix script to include proper tree

--- release/scripts/src-install.sh
+++ release/scripts/src-install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # $FreeBSD: src/release/scripts/src-install.sh,v 1.10 2004/08/06 08:42:05 cperciva Exp $
-# $MidnightBSD: src/release/scripts/src-install.sh,v 1.2 2007/03/17 16:44:45 laffer1 Exp $
+# $MidnightBSD: src/release/scripts/src-install.sh,v 1.3 2009/10/24 04:36:21 laffer1 Exp $
 
 if [ "`id -u`" != "0" ]; then
 	echo "Sorry, this must be done as root."
@@ -11,7 +11,7 @@ if [ $# -lt 1 ]; then
 	echo "You must specify which components of src to extract"
 	echo "possible subcomponents are:"
 	echo
-	echo "base bin cddl compat contrib crypto etc games gnu include krb5"
+	echo "apache base bin cddl contrib crypto etc games gnu include krb5"
 	echo "lib libexec nrelease release rescue sbin secure share sys tools"
 	echo "ubin usbin"
 	echo
@@ -20,7 +20,7 @@ if [ $# -lt 1 ]; then
 fi
 
 if [ "$1" = "all" ]; then
-	dists="base bin cddl compat contrib crypto etc games gnu include krb5 lib libexec nrelease release rescue sbin secure share sys tools ubin usbin"
+	dists="apache base bin cddl compat contrib crypto etc games gnu include krb5 lib libexec nrelease release rescue sbin secure share sys tools ubin usbin"
 else
 	dists="$*"
 fi
