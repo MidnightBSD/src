ab2bdbd29c76cce81b9e832ead02b0472916480a
Lucas Holt <luke@foolishgames.com>
2011-02-06 18:17:50 +0000

    Turn MidnightBSD ident on

--- crypto/openssh/version.h
+++ crypto/openssh/version.h
@@ -1,6 +1,12 @@
 /* $OpenBSD: version.h,v 1.60 2011/01/22 09:18:53 djm Exp $ */
 
-#define SSH_VERSION	"OpenSSH_5.7"
+#ifndef SSH_VERSION
 
-#define SSH_PORTABLE	"p1"
-#define SSH_RELEASE	SSH_VERSION SSH_PORTABLE
+#define SSH_VERSION             (ssh_version_get())
+#define SSH_RELEASE             (ssh_version_get())
+#define SSH_VERSION_BASE        "OpenSSH_5.7p1"
+#define SSH_VERSION_ADDENDUM    "MidnightBSD-20110205"
+
+const char *ssh_version_get(void);
+void ssh_version_set_addendum(const char *add);
+#endif /* SSH_VERSION */
