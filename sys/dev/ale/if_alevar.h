d3c17c9fdee4bd164aaceee7e14d3bce378886bd
Lucas Holt <luke@foolishgames.com>
2011-02-15 14:04:13 +0000

    Update ale(4) and sync with just after FreeBSD 7.3.
    Add support for WOL.

--- sys/dev/ale/if_alevar.h
+++ sys/dev/ale/if_alevar.h
@@ -24,7 +24,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $FreeBSD: src/sys/dev/ale/if_alevar.h,v 1.1.2.1.2.1 2008/11/25 02:59:29 kensmith Exp $
+ * $FreeBSD: src/sys/dev/ale/if_alevar.h,v 1.1.2.1.6.1 2010/02/10 00:26:20 kensmith Exp $
  */
 
 #ifndef	_IF_ALEVAR_H
