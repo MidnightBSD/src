a4f5f2b065a976de57f1eff6d620a6852559a68a
Lucas Holt <luke@foolishgames.com>
2011-02-19 18:36:55 +0000

    Rewrite parts of the amdtemp(4) temp monitor for K8, K10, K11 AMD CPUs to take advantage of the sensors framework.
    This is similar to coretemp(4)'s changes in MidnightBSD and to kate(4) in OpenBSD/DragonFly.  However, kate(4) only handles K8 CPUs!
    
    I don't have any K8 hardware to test on so if the sensor readings are off, I need someone to let me know.
    
    Tested on an AMD Phenom 9600

--- sys/dev/amdtemp/amdtemp.c
+++ sys/dev/amdtemp/amdtemp.c
@@ -1,6 +1,7 @@
 /*-
  * Copyright (c) 2008, 2009 Rui Paulo <rpaulo@FreeBSD.org>
  * Copyright (c) 2009 Norikatsu Shigemura <nork@FreeBSD.org>
+ * Copyright (c) 2011 Lucas Holt <luke@foolishgames.com>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -31,7 +32,7 @@
  */
 
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: src/sys/dev/amdtemp/amdtemp.c,v 1.3.2.1 2009/05/21 17:09:23 rpaulo Exp $");
+__MBSDID("$MidnightBSD$");
 
 #include <sys/param.h>
 #include <sys/bus.h>
@@ -40,7 +41,7 @@ __FBSDID("$FreeBSD: src/sys/dev/amdtemp/amdtemp.c,v 1.3.2.1 2009/05/21 17:09:23 
 #include <sys/module.h>
 #include <sys/conf.h>
 #include <sys/kernel.h>
-#include <sys/sysctl.h>
+#include <sys/sensors.h>
 
 #include <machine/specialreg.h>
 #include <machine/cpufunc.h>
@@ -48,6 +49,8 @@ __FBSDID("$FreeBSD: src/sys/dev/amdtemp/amdtemp.c,v 1.3.2.1 2009/05/21 17:09:23 
 
 #include <dev/pci/pcireg.h>
 #include <dev/pci/pcivar.h>
+
+extern int smp_cpus;
 
 typedef enum {
 	SENSOR0_CORE0,
@@ -59,12 +62,11 @@ typedef enum {
 } amdsensor_t;
 
 struct amdtemp_softc {
-	device_t	sc_dev;
+	struct ksensordev	sc_sensordev;
+	struct ksensor		sc_sensors[4];
+	device_t		sc_dev;
 	int		sc_temps[4];
 	int		sc_ntemps;
-	struct sysctl_oid *sc_oid;
-	struct sysctl_oid *sc_sysctl_cpu[2];
-	struct intr_config_hook sc_ich;
 	int32_t (*sc_gettemp)(device_t, amdsensor_t);
 };
 
@@ -95,10 +97,10 @@ static struct amdtemp_product {
  */
 #define	AMDTEMP_REG		0xa4
 
-#define	TZ_ZEROC		2732
+#define	TZ_ZEROC		27315
 
-					/* -49 C is the mininum temperature */
-#define	AMDTEMP_OFFSET0F	(TZ_ZEROC-490)
+				/* -49 C is the mininum temperature */
+#define	AMDTEMP_OFFSET0F	(TZ_ZEROC-4900)
 #define	AMDTEMP_OFFSET		(TZ_ZEROC)
 
 /*
@@ -107,12 +109,11 @@ static struct amdtemp_product {
 static void 	amdtemp_identify(driver_t *driver, device_t parent);
 static int	amdtemp_probe(device_t dev);
 static int	amdtemp_attach(device_t dev);
-static void	amdtemp_intrhook(void *arg);
 static int	amdtemp_detach(device_t dev);
 static int 	amdtemp_match(device_t dev);
 static int32_t	amdtemp_gettemp0f(device_t dev, amdsensor_t sensor);
 static int32_t	amdtemp_gettemp(device_t dev, amdsensor_t sensor);
-static int	amdtemp_sysctl(SYSCTL_HANDLER_ARGS);
+static void	amdtemp_refresh(void *arg);
 
 static device_method_t amdtemp_methods[] = {
 	/* Device interface */
@@ -159,7 +160,11 @@ amdtemp_identify(driver_t *driver, device_t parent)
 	/* Make sure we're not being doubly invoked. */
 	if (device_find_child(parent, "amdtemp", -1) != NULL)
 		return;
-	
+
+	/* Make sure AMD is the vendor */
+	if (strcmp(cpu_vendor, "AuthenticAMD"))
+		return;
+
 	if (amdtemp_match(parent)) {
 		child = device_add_child(parent, "amdtemp", -1);
 		if (child == NULL)
@@ -192,144 +197,48 @@ static int
 amdtemp_attach(device_t dev)
 {
 	struct amdtemp_softc *sc = device_get_softc(dev);
-	struct sysctl_ctx_list *sysctlctx;
-	struct sysctl_oid *sysctlnode;
+	device_t pdev;
+	int i;
 
+	sc->sc_dev = dev;
+	pdev = device_get_parent(dev);
 
-	/*
-	 * Setup intrhook function to create dev.cpu sysctl entries. This is
-	 * needed because the cpu driver may be loaded late on boot, after
-	 * us.
-	 */
-	sc->sc_ich.ich_func = amdtemp_intrhook;
-	sc->sc_ich.ich_arg = dev;
-	if (config_intrhook_establish(&sc->sc_ich) != 0) {
-		device_printf(dev, "config_intrhook_establish "
-		    "failed!\n");
-		return (ENXIO);
-	}
-	
 	if (pci_get_device(dev) == DEVICEID_AMD_MISC0F)
 		sc->sc_gettemp = amdtemp_gettemp0f;
-	else {
+	else
 		sc->sc_gettemp = amdtemp_gettemp;
-		return (0);
-	}
+
+	sc->sc_ntemps = 2;
 
 	/*
-	 * dev.amdtemp.N tree.
+	 * Add hw.sensors.cpuN.temp0 MIB.
 	 */
-	sysctlctx = device_get_sysctl_ctx(dev);
-	sysctlnode = SYSCTL_ADD_NODE(sysctlctx,
-	    SYSCTL_CHILDREN(device_get_sysctl_tree(dev)), OID_AUTO, "sensor0",
-	    CTLFLAG_RD, 0, "Sensor 0");
-	
-	SYSCTL_ADD_PROC(sysctlctx,
-	    SYSCTL_CHILDREN(sysctlnode),
-	    OID_AUTO, "core0", CTLTYPE_INT | CTLFLAG_RD,
-	    dev, SENSOR0_CORE0, amdtemp_sysctl, "IK",
-	    "Sensor 0 / Core 0 temperature");
-	
-	SYSCTL_ADD_PROC(sysctlctx,
-	    SYSCTL_CHILDREN(sysctlnode),
-	    OID_AUTO, "core1", CTLTYPE_INT | CTLFLAG_RD,
-	    dev, SENSOR0_CORE1, amdtemp_sysctl, "IK",
-	    "Sensor 0 / Core 1 temperature");
-	
-	sysctlnode = SYSCTL_ADD_NODE(sysctlctx,
-	    SYSCTL_CHILDREN(device_get_sysctl_tree(dev)), OID_AUTO, "sensor1",
-	    CTLFLAG_RD, 0, "Sensor 1");
-	
-	SYSCTL_ADD_PROC(sysctlctx,
-	    SYSCTL_CHILDREN(sysctlnode),
-	    OID_AUTO, "core0", CTLTYPE_INT | CTLFLAG_RD,
-	    dev, SENSOR1_CORE0, amdtemp_sysctl, "IK",
-	    "Sensor 1 / Core 0 temperature");
-	
-	SYSCTL_ADD_PROC(sysctlctx,
-	    SYSCTL_CHILDREN(sysctlnode),
-	    OID_AUTO, "core1", CTLTYPE_INT | CTLFLAG_RD,
-	    dev, SENSOR1_CORE1, amdtemp_sysctl, "IK",
-	    "Sensor 1 / Core 1 temperature");
+	strlcpy(sc->sc_sensordev.xname, device_get_nameunit(pdev),
+	    sizeof(sc->sc_sensordev.xname));
 
-	return (0);
-}
-
-void
-amdtemp_intrhook(void *arg)
-{
-	int i;
-	device_t nexus, acpi, cpu;
-	device_t dev = (device_t) arg;
-	struct amdtemp_softc *sc;
-	struct sysctl_ctx_list *sysctlctx;
-
-	sc = device_get_softc(dev);
-	
-	/*
-	 * dev.cpu.N.temperature.
-	 */
-	nexus = device_find_child(root_bus, "nexus", 0);
-	acpi = device_find_child(nexus, "acpi", 0);
-
-	for (i = 0; i < 2; i++) {
-		cpu = device_find_child(acpi, "cpu",
-		    device_get_unit(dev) * 2 + i);
-		if (cpu) {
-			sysctlctx = device_get_sysctl_ctx(cpu);
-
-			sc->sc_sysctl_cpu[i] = SYSCTL_ADD_PROC(sysctlctx,
-			    SYSCTL_CHILDREN(device_get_sysctl_tree(cpu)),
-			    OID_AUTO, "temperature", CTLTYPE_INT | CTLFLAG_RD,
-			    dev, CORE0, amdtemp_sysctl, "IK",
-			    "Max of sensor 0 / 1");
-		}
-	}
-	config_intrhook_disestablish(&sc->sc_ich);
-}
-
-int
-amdtemp_detach(device_t dev)
-{
-	int i;
-	struct amdtemp_softc *sc = device_get_softc(dev);
-	
-	for (i = 0; i < 2; i++) {
-		if (sc->sc_sysctl_cpu[i])
-			sysctl_remove_oid(sc->sc_sysctl_cpu[i], 1, 0);
+	for (i = 0; i < sc->sc_ntemps; i++) {
+		sc->sc_sensors[i].type = SENSOR_TEMP;
+		sensor_attach(&sc->sc_sensordev, &sc->sc_sensors[i]);
 	}
 
-	/* NewBus removes the dev.amdtemp.N tree by itself. */
-	
+	if (sensor_task_register(sc, amdtemp_refresh, 5)) {
+		device_printf(dev, "unable to register update task\n");
+		return (ENXIO);
+	}
+	sensordev_install(&sc->sc_sensordev);
+
 	return (0);
 }
 
 static int
-amdtemp_sysctl(SYSCTL_HANDLER_ARGS)
+amdtemp_detach(device_t dev)
 {
-	device_t dev = (device_t) arg1;
 	struct amdtemp_softc *sc = device_get_softc(dev);
-	int error;
-	int32_t temp, auxtemp[2];
 
-	switch (arg2) {
-	case CORE0:
-		auxtemp[0] = sc->sc_gettemp(dev, SENSOR0_CORE0);
-		auxtemp[1] = sc->sc_gettemp(dev, SENSOR1_CORE0);
-		temp = imax(auxtemp[0], auxtemp[1]);
-		break;
-	case CORE1:
-		auxtemp[0] = sc->sc_gettemp(dev, SENSOR0_CORE1);
-		auxtemp[1] = sc->sc_gettemp(dev, SENSOR1_CORE1);
-		temp = imax(auxtemp[0], auxtemp[1]);
-		break;
-	default:
-		temp = sc->sc_gettemp(dev, arg2);
-		break;
-	}
-	error = sysctl_handle_int(oidp, &temp, 0, req);
+	sensordev_deinstall(&sc->sc_sensordev);
+	sensor_task_unregister(sc);
 	
-	return (error);
+	return (0);
 }
 
 static int32_t
@@ -360,7 +269,7 @@ amdtemp_gettemp0f(device_t dev, amdsensor_t sensor)
 	}
 	pci_write_config(dev, AMDTEMP_REG0F, cfg, 1);
 	temp = pci_read_config(dev, AMDTEMP_REG0F, 4);
-	temp = ((temp >> 16) & 0xff) * 10 + AMDTEMP_OFFSET0F;
+	temp = ((temp >> 16) & 0xff) * 100 + AMDTEMP_OFFSET0F;
 	
 	return (temp);
 }
@@ -371,7 +280,37 @@ amdtemp_gettemp(device_t dev, amdsensor_t sensor)
 	uint32_t temp;
 
 	temp = pci_read_config(dev, AMDTEMP_REG, 4);
-	temp = ((temp >> 21) & 0x3ff) * 10 / 8 + AMDTEMP_OFFSET;
-
+	temp = (temp >> 21) * 100 / 8 + AMDTEMP_OFFSET;
 	return (temp);
+}
+
+static void
+amdtemp_refresh(void *arg)
+{
+	struct amdtemp_softc *sc = arg;
+	device_t dev = sc->sc_dev;
+	struct ksensor *s = sc->sc_sensors;
+	int32_t temp, auxtemp[2];
+	int i;
+
+	for (i = 0; i < sc->sc_ntemps; i++)
+	{
+		switch (i) {
+			case 0:
+				auxtemp[0] = sc->sc_gettemp(dev, SENSOR0_CORE0);
+				auxtemp[1] = sc->sc_gettemp(dev, SENSOR1_CORE0);
+				temp = imax(auxtemp[0], auxtemp[1]);
+				break;
+			case 1:
+				auxtemp[0] = sc->sc_gettemp(dev, SENSOR0_CORE1);
+				auxtemp[1] = sc->sc_gettemp(dev, SENSOR1_CORE1);
+				temp = imax(auxtemp[0], auxtemp[1]);
+				break;
+			default:
+				temp = sc->sc_gettemp(dev, CORE0);
+				break;
+		}
+		s[i].flags &= ~SENSOR_FINVALID;
+		s[i].value = temp * 10000;
+	}
 }
