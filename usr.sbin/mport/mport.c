cb7c823ea12030f2a68baa8f0af85d44bf69725f
Lucas Holt <luke@foolishgames.com>
2011-03-06 17:20:25 +0000

    Major refactor of mport tool code.
    Use libmport to lookup entries in the index for fetching remote packages.  This means we can now create a magus run and load it on the ftp and have users remotely install mport packages!  (local install is a todo item)
    
    Add a new update command which uninstalls a package and then fetches and installs a new one.  This is very "dumb" right now and it doesn't do updepends checks yet.

--- usr.sbin/mport/mport.c
+++ usr.sbin/mport/mport.c
@@ -22,47 +22,47 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- * $MidnightBSD: src/usr.sbin/mport/mport.c,v 1.7 2011/02/27 17:15:29 laffer1 Exp $
  */
 
 #include <sys/cdefs.h>
-__MBSDID("$MidnightBSD: src/usr.sbin/mport/mport.c,v 1.7 2011/02/27 17:15:29 laffer1 Exp $");
+__MBSDID("$MidnightBSD: src/usr.sbin/mport/mport.c,v 1.8 2011/03/06 00:40:02 laffer1 Exp $");
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <mport.h>
 
 #define MPORT_TOOLS_PATH "/usr/libexec/"
-#define MPORT_LOCAL_PKG_PATH "/usr/mports/Packages"
+#define MPORT_LOCAL_PKG_PATH "/var/db/mport/downloads"
 
-void usage(void);
+static void usage(void);
+static mportIndexEntry ** lookupIndex(mportInstance *mport, const char *packageName);
+static int install(mportInstance *mport, const char *packageName);
+static int delete(mportInstance *mport, const char *packageName);
+static int update(mportInstance *mport, const char *packageName);
 
 int 
 main(int argc, char *argv[]) {
 	char *buf = NULL;
+	mportInstance *mport;
+	int resultCode;
 
 	if (argc < 2)
 		usage();
 
+	mport = mport_instance_new();
+
+	if (mport_instance_init(mport, NULL) != MPORT_OK) {
+		warnx("%s", mport_err_string());
+		exit(1);
+	}
+
 	if (!strcmp(argv[1], "install")) {
-		asprintf(&buf, "%s%s %s/%s/All/%s.mport",
-			MPORT_TOOLS_PATH,
-			"mport.install",
-			MPORT_LOCAL_PKG_PATH,
-#if defined(__i386__)
-			"i386",
-#elif defined(__amd64__)
-			"amd64",
-#else
-			"sparc64",
-#endif
-			argv[2]); 	
+		resultCode = install(mport, argv[2]);
 	} else if (!strcmp(argv[1], "delete")) {
-		asprintf(&buf, "%s%s %s",
-			MPORT_TOOLS_PATH,
-			"mport.delete -n",
-			argv[2]);
+		resultCode = delete(mport, argv[2]);
+	} else if (!strcmp(argv[1], "update")) {
+		resultCode = update(mport, argv[2]);
         } else if (!strcmp(argv[1], "list")) {
 		if (argc > 2 && !strcmp(argv[2], "updates"))
 			asprintf(&buf, "%s%s",
@@ -72,16 +72,16 @@ main(int argc, char *argv[]) {
 			asprintf(&buf, "%s%s",
                         	MPORT_TOOLS_PATH,
                         	"mport.list -v");
+		system(buf);
+		free(buf);
+		resultCode = 0;
 	} else {
+		mport_instance_free(mport);
 		usage();
 	}
 
-#ifdef DEBUG
-	printf("command: %s\n", buf);
-#endif
-	system(buf);
-
-	return 0;
+	mport_instance_free(mport);
+	return resultCode;
 }
 
 void
@@ -90,7 +90,72 @@ usage(void) {
 		"usage: mport <command> args:\n"
 		"       mport delete [package name]\n"
 		"       mport install [package name]\n"
+		"	mport update [package name]\n"
 		"       mport list [updates]\n"
 	);
 	exit(1);
+}
+
+mportIndexEntry **
+lookupIndex(mportInstance *mport, const char *packageName)
+{
+	mportIndexEntry **indexEntries;
+
+	if (mport_index_load(mport) != MPORT_OK)
+		errx(4, "Unable to load updates index");
+	
+	if (mport_index_lookup_pkgname(mport, packageName, &indexEntries) != MPORT_OK) {
+		fprintf(stderr, "Error looking up package name %s: %d %s\n", packageName,  mport_err_code(), mport_err_string());
+		exit(mport_err_code());
+	}
+
+	return indexEntries;
+}
+
+int
+install(mportInstance *mport, const char *packageName) {
+	char *buf;
+	mportIndexEntry **indexEntry;
+
+	indexEntry = lookupIndex(mport, packageName);
+	if (indexEntry == NULL || *indexEntry == NULL)
+		return 1;
+
+	/* TODO: verify package isn't already downloaded */
+	if (mport_fetch_bundle(mport, (*indexEntry)->bundlefile) != MPORT_OK) {
+		fprintf(stderr, "%s\n", mport_err_string());
+		exit(mport_err_code());
+	}
+
+	asprintf(&buf, "%s%s %s/%s",
+			MPORT_TOOLS_PATH,
+			"mport.install",
+			MPORT_LOCAL_PKG_PATH,
+			(*indexEntry)->bundlefile);
+	system(buf);
+	free(buf);
+	mport_index_entry_free_vec(indexEntry);
+
+	return 0;
+}
+
+int
+delete(mportInstance *mport, const char *packageName) {
+	char *buf;
+
+	asprintf(&buf, "%s%s %s",
+			MPORT_TOOLS_PATH,
+			"mport.delete -n",
+			packageName);
+	system(buf);
+	free(buf);
+
+	return 0;
+}
+
+int
+update(mportInstance *mport, const char *packageName) {
+	/* TODO: verify installed, do updepends */
+	delete(mport, packageName);
+	return install(mport, packageName);
 } 
