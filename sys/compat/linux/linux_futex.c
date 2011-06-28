87e669e39e118ff6c7c65deafab1a9d6238b372a
Lucas Holt <luke@foolishgames.com>
2011-06-28 02:22:32 +0000

    remove unnecessary includes

--- sys/compat/linux/linux_futex.c
+++ sys/compat/linux/linux_futex.c
@@ -1,4 +1,4 @@
-/* $MidnightBSD: src/sys/compat/linux/linux_futex.c,v 1.5 2011/06/22 23:56:22 laffer1 Exp $ */
+/* $MidnightBSD: src/sys/compat/linux/linux_futex.c,v 1.6 2011/06/23 12:41:19 laffer1 Exp $ */
 /*	$NetBSD: linux_futex.c,v 1.7 2006/07/24 19:01:49 manu Exp $ */
 
 /*-
@@ -59,9 +59,6 @@ __KERNEL_RCSID(1, "$NetBSD: linux_futex.c,v 1.7 2006/07/24 19:01:49 manu Exp $")
 #include <machine/../linux/linux_proto.h>
 #endif
 #include <compat/linux/linux_futex.h>
-#include <compat/linux/linux_emul.h>
-#include <compat/linux/linux_util.h>
-
 struct futex;
 
 struct waiting_proc {
