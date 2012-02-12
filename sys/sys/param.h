f7e82de0e52d688acb361cd8f1c9741fd10ef3a6
Lucas Holt <luke@foolishgames.com>
2012-02-12 21:16:00 +0000

    bump MidnightBSD version for support for preinit, init and fini arrays and related chapges to csu and rtld-elf

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -33,7 +33,7 @@
  *
  *	@(#)param.h	8.3 (Berkeley) 4/4/95
  * $FreeBSD: src/sys/sys/param.h,v 1.244.2.8 2006/01/18 13:24:24 andre Exp $
- * $MidnightBSD: src/sys/sys/param.h,v 1.44 2011/12/18 03:59:06 laffer1 Exp $
+ * $MidnightBSD: src/sys/sys/param.h,v 1.45 2011/12/28 03:14:45 laffer1 Exp $
  */
 
 #ifndef _SYS_PARAM_H_
@@ -61,7 +61,7 @@
 #define __FreeBSD_version 700055
 
 #undef __MidnightBSD_version
-#define __MidnightBSD_version 4013	/* Master, propagated to newvers */
+#define __MidnightBSD_version 4014	/* Master, propagated to newvers */
 
 #ifndef LOCORE
 #include <sys/types.h>
