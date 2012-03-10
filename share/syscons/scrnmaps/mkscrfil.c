5f31cb81ab539d99fa7da14ed685284e871e65ab
Lucas Holt <luke@foolishgames.com>
2012-03-10 03:43:55 +0000

    drop isdn.
    update coldef, mklocale, monentdef, numericdef, ... locate related stuff
    
    Update syscons keymaps, fonts

--- share/syscons/scrnmaps/mkscrfil.c
+++ share/syscons/scrnmaps/mkscrfil.c
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $FreeBSD: src/share/syscons/scrnmaps/mkscrfil.c,v 1.2 2000/10/08 21:33:52 phk Exp $
+ * $MidnightBSD$
  */
 
 #include <sys/ioctl.h>
