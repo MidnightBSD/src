fbab863f1ea0e789e0d8a40fc0ad804b19de787c
Lucas Holt <luke@foolishgames.com>
2012-03-16 03:29:19 +0000

    make this easier to read

--- sys/sys/semaphore.h
+++ sys/sys/semaphore.h
@@ -1,4 +1,4 @@
-/* $MidnightBSD$ */
+/* $MidnightBSD: src/sys/sys/semaphore.h,v 1.2 2008/12/03 00:11:23 laffer1 Exp $ */
 /*-
  * Copyright (c) 1996, 1997
  *	HD Associates, Inc.  All rights reserved.
@@ -38,12 +38,14 @@
 #ifndef _SEMAPHORE_H_
 #define _SEMAPHORE_H_
 
+#include <machine/_limits.h>
+
 /* Opaque type definition. */
 struct sem;
 typedef	struct sem *	sem_t;
 
 #define	SEM_FAILED	((sem_t *)0)
-#define	SEM_VALUE_MAX	(~0U)		/* Equivalent to UINT_MAX. */
+#define	SEM_VALUE_MAX	__INT_MAX
 
 #ifndef _KERNEL
 #include <sys/cdefs.h>
