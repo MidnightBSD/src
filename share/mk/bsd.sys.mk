f82041c04e5fe17d8d71dd8e9bc45e8b83d28c72
Lucas Holt <luke@foolishgames.com>
2012-06-09 23:10:21 +0000

    Formatting.

--- share/mk/bsd.sys.mk
+++ share/mk/bsd.sys.mk
@@ -1,63 +1,63 @@
 # $FreeBSD: src/share/mk/bsd.sys.mk,v 1.37 2005/01/16 21:18:16 obrien Exp $
-# $MidnightBSD: src/share/mk/bsd.sys.mk,v 1.10 2011/10/26 16:23:34 laffer1 Exp $
+# $MidnightBSD: src/share/mk/bsd.sys.mk,v 1.11 2012/03/10 01:03:15 laffer1 Exp $
 #
-# This file contains common settings used for building FreeBSD
+# This file contains common settings used for building MidnightBSD
 # sources.
 
 # Enable various levels of compiler warning checks.  These may be
 # overridden (e.g. if using a non-gcc compiler) by defining NO_WARNS.
 
-# for GCC:  http://gcc.gnu.org/onlinedocs/gcc-3.0.4/gcc_3.html#IDX143
+# for GCC:  http://gcc.gnu.org/onlinedocs/gcc-4.2.1/gcc/Warning-Options.html
 
 # the default is gnu99 for now
-CSTD		?= gnu99
+CSTD?=		gnu99
 
 .if ${CSTD} == "k&r"
-CFLAGS		+= -traditional
+CFLAGS+=	-traditional
 .elif ${CSTD} == "c89" || ${CSTD} == "c90"
-CFLAGS		+= -std=iso9899:1990
+CFLAGS+=	-std=iso9899:1990
 .elif ${CSTD} == "c94" || ${CSTD} == "c95"
-CFLAGS		+= -std=iso9899:199409
+CFLAGS+=	-std=iso9899:199409
 .elif ${CSTD} == "c99"
-CFLAGS		+= -std=iso9899:1999
+CFLAGS+=	-std=iso9899:1999
 .else
-CFLAGS		+= -std=${CSTD}
+CFLAGS+=	-std=${CSTD}
 .endif
 .if !defined(NO_WARNS)
 # -pedantic is problematic because it also imposes namespace restrictions
-#CFLAGS		+= -pedantic
+#CFLAGS+=	-pedantic
 . if defined(WARNS)
 .  if ${WARNS} >= 1
-CWARNFLAGS	+=	-Wsystem-headers
+CWARNFLAGS+=	-Wsystem-headers
 .   if !defined(NO_WERROR)
-CWARNFLAGS	+=	-Werror
+CWARNFLAGS+=	-Werror
 .   endif
 .  endif
 .  if ${WARNS} >= 2
-CWARNFLAGS	+=	-Wall -Wno-format-y2k
+CWARNFLAGS+=	-Wall -Wno-format-y2k
 .  endif
 .  if ${WARNS} >= 3
-CWARNFLAGS	+=	-W -Wno-unused-parameter -Wstrict-prototypes\
+CWARNFLAGS+=	-W -Wno-unused-parameter -Wstrict-prototypes\
 			-Wmissing-prototypes -Wpointer-arith
 .  endif
 .  if ${WARNS} >= 4
-CWARNFLAGS	+=	-Wreturn-type -Wcast-qual -Wwrite-strings -Wswitch\
+CWARNFLAGS+=	-Wreturn-type -Wcast-qual -Wwrite-strings -Wswitch\
 			-Wshadow -Wunused-parameter
 .   if !defined(NO_WCAST_ALIGN)
-CWARNFLAGS	+=	-Wcast-align
+CWARNFLAGS+=	-Wcast-align
 .   endif
 .  endif
 # BDECFLAGS
 .  if ${WARNS} >= 6
-CWARNFLAGS	+=	-Wchar-subscripts -Winline -Wnested-externs\
+CWARNFLAGS+=	-Wchar-subscripts -Winline -Wnested-externs\
 			-Wredundant-decls -Wold-style-definition
 .  endif
 .  if ${WARNS} >= 2 && ${WARNS} <= 4
 # XXX Delete -Wuninitialized by default for now -- the compiler doesn't
 # XXX always get it right.
-CWARNFLAGS	+=	-Wno-uninitialized
+CWARNFLAGS+=	-Wno-uninitialized
 .  endif
-CWARNFLAGS	+=	-Wno-pointer-sign
+CWARNFLAGS+=	-Wno-pointer-sign
 . endif
 
 . if defined(FORMAT_AUDIT)
@@ -65,26 +65,26 @@ WFORMAT		=	1
 . endif
 . if defined(WFORMAT)
 .  if ${WFORMAT} > 0
-#CWARNFLAGS	+=	-Wformat-nonliteral -Wformat-security -Wno-format-extra-args
-CWARNFLAGS	+=	-Wformat=2 -Wno-format-extra-args
+#CWARNFLAGS+=	-Wformat-nonliteral -Wformat-security -Wno-format-extra-args
+CWARNFLAGS+=	-Wformat=2 -Wno-format-extra-args
 .   if !defined(NO_WERROR)
-CWARNFLAGS	+=	-Werror
+CWARNFLAGS+=	-Werror
 .   endif
 .  endif
 . endif
 . if defined(NO_WFORMAT)
-CWARNFLAGS	+=	-Wno-format
+CWARNFLAGS+=	-Wno-format
 . endif
 .endif
 
 .if defined(IGNORE_PRAGMA)
-CWARNFLAGS	+=	-Wno-unknown-pragmas
+CWARNFLAGS+=	-Wno-unknown-pragmas
 .endif
 
 .if ${MK_SSP} != "no"
-SSP_CFLAGS	?=	-fstack-protector
-CFLAGS		+=	${SSP_CFLAGS}
+SSP_CFLAGS?=	-fstack-protector
+CFLAGS+=	${SSP_CFLAGS}
 .endif
 
 # Allow user-specified additional warning flags
-CFLAGS		+=	${CWARNFLAGS}
+CFLAGS+=	${CWARNFLAGS}
