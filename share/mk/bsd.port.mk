f99c6e042d1f454e01ad4f9d62250fa6ad0037e9
Lucas Holt <luke@foolishgames.com>
2012-06-15 03:38:06 +0000

    teach ports about ctf

--- share/mk/bsd.port.mk
+++ share/mk/bsd.port.mk
@@ -1,5 +1,5 @@
 # $FreeBSD: src/share/mk/bsd.port.mk,v 1.307 2004/07/02 20:47:18 eik Exp $
-# $MidnightBSD: src/share/mk/bsd.port.mk,v 1.6 2007/04/15 04:55:21 laffer1 Exp $
+# $MidnightBSD: src/share/mk/bsd.port.mk,v 1.7 2008/10/14 21:13:54 laffer1 Exp $
 
 PORTSDIR?=	/usr/mports
 BSDPORTMK?=	${PORTSDIR}/Mk/bsd.mport.mk
@@ -7,6 +7,11 @@ BSDPORTMK?=	${PORTSDIR}/Mk/bsd.mport.mk
 # Needed to keep bsd.own.mk from reading in /etc/src.conf
 # and setting MK_* variables when building ports.
 _WITHOUT_SRCCONF=
+
+# Enable CTF conversion on request.
+.if defined(WITH_CTF)
+.undef NO_CTF
+.endif
 
 .include <bsd.own.mk>
 .include "${BSDPORTMK}"
