ec6e37cb0bf26ab21e119bcb458477e27afb208b
Lucas Holt <luke@foolishgames.com>
2012-07-08 15:53:49 +0000

    fix some problems with tcsh seg faulting after import of iconv

--- bin/csh/iconv_stub.h
+++ bin/csh/iconv_stub.h
@@ -23,59 +23,23 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $FreeBSD: src/bin/csh/iconv_stub.c,v 1.1.2.1 2006/02/26 06:17:59 ume Exp $
- * $MidnightBSD: src/bin/csh/iconv_stub.c,v 1.1 2006/12/31 09:09:40 laffer1 Exp $
+ * $MidnightBSD$
+ * $FreeBSD: src/bin/csh/iconv_stub.h,v 1.1 2011/02/26 18:54:54 ume Exp $
  */
 
-#include <dlfcn.h>
-#include <stddef.h>
+#ifndef _ICONV_H_
+#define _ICONV_H_
 
-#include "iconv.h"
+typedef void *iconv_t;
+typedef size_t dl_iconv_t(iconv_t, const char **, size_t *, char **, size_t *);
+typedef int dl_iconv_close_t(iconv_t);
 
-#undef iconv_open
-#undef iconv
-#undef iconv_close
+extern iconv_t dl_iconv_open(const char *, const char *);
+extern dl_iconv_t *dl_iconv;
+extern dl_iconv_close_t *dl_iconv_close;
 
-#define ICONVLIB	"libiconv.so"
-#define ICONV_ENGINE	"iconv"
-#define ICONV_OPEN	"iconv_open"
-#define ICONV_CLOSE	"iconv_close"
+#define iconv_open	dl_iconv_open
+#define iconv		dl_iconv
+#define iconv_close	dl_iconv_close
 
-typedef iconv_t iconv_open_t(const char *, const char *);
-
-dl_iconv_t *dl_iconv;
-dl_iconv_close_t *dl_iconv_close;
-
-static int initialized;
-static void *iconvlib;
-static iconv_open_t *iconv_open;
-
-iconv_t
-dl_iconv_open(const char *tocode, const char *fromcode)
-{
-	if (initialized) {
-		if (iconvlib == NULL)
-			return (iconv_t)-1;
-	} else {
-		initialized = 1;
-		iconvlib = dlopen(ICONVLIB, RTLD_LAZY | RTLD_GLOBAL);
-		if (iconvlib == NULL)
-			return (iconv_t)-1;
-		iconv_open = (iconv_open_t *)dlfunc(iconvlib, ICONV_OPEN);
-		if (iconv_open == NULL)
-			goto dlfunc_err;
-		dl_iconv = (dl_iconv_t *)dlfunc(iconvlib, ICONV_ENGINE);
-		if (dl_iconv == NULL)
-			goto dlfunc_err;
-		dl_iconv_close = (dl_iconv_close_t *)dlfunc(iconvlib,
-		    ICONV_CLOSE);
-		if (dl_iconv_close == NULL)
-			goto dlfunc_err;
-	}
-	return iconv_open(tocode, fromcode);
-
-dlfunc_err:
-	dlclose(iconvlib);
-	iconvlib = NULL;
-	return (iconv_t)-1;
-}
+#endif /* !_ICONV_H_ */
