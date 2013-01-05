ea8a1f7869ab568764c5a30594516e6fb564305d
Lucas Holt <luke@foolishgames.com>
2013-01-05 17:43:45 +0000

    hack up

--- contrib/tzcode/private.h
+++ contrib/tzcode/private.h
@@ -5,7 +5,22 @@
 /*
 ** This file is in the public domain, so clarified as of
 ** 1996-06-05 by Arthur David Olson.
+**
+** $FreeBSD$
 */
+
+/* Stuff moved from Makefile.inc to reduce clutter */
+#ifndef TM_GMTOFF
+#define TM_GMTOFF	tm_gmtoff
+#define TM_ZONE		tm_zone
+#define STD_INSPIRED	1
+#define PCTS		1
+#define HAVE_LONG_DOUBLE 1
+#define HAVE_STRERROR	1
+#define	HAVE_UNISTD_H	1
+#define	LOCALE_HOME	_PATH_LOCALE
+#define TZDIR		"/usr/share/zoneinfo"
+#endif /* ndef TM_GMTOFF */
 
 /*
 ** This header is for use ONLY with the time conversion code.
@@ -14,6 +29,18 @@
 ** Do NOT copy it to any system include directory.
 ** Thank you!
 */
+
+/*
+** ID
+*/
+
+#ifndef lint
+#ifndef NOID
+/*
+static char	privatehid[] = "@(#)private.h	8.6";
+*/
+#endif /* !defined NOID */
+#endif /* !defined lint */
 
 #define GRANDPARENTED	"Local time zone must be set--see zic manual page"
 
@@ -98,12 +125,14 @@
 #include "unistd.h"	/* for F_OK, R_OK, and other POSIX goodness */
 #endif /* HAVE_UNISTD_H */
 
+#if !(HAVE_UNISTD_H)
 #ifndef F_OK
 #define F_OK	0
 #endif /* !defined F_OK */
 #ifndef R_OK
 #define R_OK	4
 #endif /* !defined R_OK */
+#endif /* !(HAVE_UNISTD_H) */
 
 /* Unlike <ctype.h>'s isdigit, this also works if c < 0 | c > UCHAR_MAX. */
 #define is_digit(c) ((unsigned)(c) - '0' <= 9)
@@ -144,12 +173,6 @@ typedef long		int_fast64_t;
 #define INT32_MIN (-1 - INT32_MAX)
 #endif /* !defined INT32_MIN */
 
-#if 2 < __GNUC__ || (__GNUC__ == 2 && 96 <= __GNUC_MINOR__)
-# define ATTRIBUTE_PURE __attribute__ ((__pure__))
-#else
-# define ATTRIBUTE_PURE /* empty */
-#endif
-
 /*
 ** Workarounds for compilers/systems.
 */
@@ -168,8 +191,13 @@ extern char *	asctime_r(struct tm const *, char *);
 ** Private function declarations.
 */
 
+char *		icalloc(int nelem, int elsize);
 char *		icatalloc(char * old, const char * new);
 char *		icpyalloc(const char * string);
+char *		imalloc(int n);
+void *		irealloc(void * pointer, int size);
+void		icfree(char * pointer);
+void		ifree(char * pointer);
 const char *	scheck(const char * string, const char * format);
 
 /*
@@ -191,6 +219,15 @@ const char *	scheck(const char * string, const char * format);
 #ifndef TYPE_SIGNED
 #define TYPE_SIGNED(type) (((type) -1) < 0)
 #endif /* !defined TYPE_SIGNED */
+
+/*
+** Since the definition of TYPE_INTEGRAL contains floating point numbers,
+** it cannot be used in preprocessor directives.
+*/
+
+#ifndef TYPE_INTEGRAL
+#define	TYPE_INTEGRAL(type) (((type) 0.5) != 0.5)
+#endif /* !defined TYPE_INTEGRAL */
 
 /*
 ** Since the definition of TYPE_INTEGRAL contains floating point numbers,
@@ -261,7 +298,7 @@ const char *	scheck(const char * string, const char * format);
 char *asctime_r(struct tm const *, char *);
 char *ctime_r(time_t const *, char *);
 #endif /* HAVE_INCOMPATIBLE_CTIME_R */
-
+  
 #ifndef YEARSPERREPEAT
 #define YEARSPERREPEAT		400	/* years before a Gregorian repeat */
 #endif /* !defined YEARSPERREPEAT */
