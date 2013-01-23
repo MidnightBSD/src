ab5c1cb990a55c50c916118b33619af2bfbf7ab3
Lucas Holt <luke@foolishgames.com>
2013-01-23 02:50:19 +0000

    tag

--- usr.sbin/memcontrol/memcontrol.c
+++ usr.sbin/memcontrol/memcontrol.c
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $FreeBSD: src/usr.sbin/memcontrol/memcontrol.c,v 1.10 2005/03/29 20:17:47 anholt Exp $
+ * $MidnightBSD$
  */
 
 #include <sys/types.h>
