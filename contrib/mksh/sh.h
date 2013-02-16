fa42817f658ced457180555853ba47500ebff1a7
Lucas Holt <luke@foolishgames.com>
2013-02-16 00:18:24 +0000

    merge

--- contrib/mksh/sh.h
+++ contrib/mksh/sh.h
@@ -164,9 +164,9 @@
 #endif
 
 #ifdef EXTERN
-__RCSID("$MirOS: src/bin/mksh/sh.h,v 1.634.2.1 2013/02/11 17:25:03 tg Exp $");
+__RCSID("$MirOS: src/bin/mksh/sh.h,v 1.635 2013/02/15 18:50:14 tg Exp $");
 #endif
-#define MKSH_VERSION "R42 2013/02/10"
+#define MKSH_VERSION "R42 2013/02/15"
 
 /* arithmetic types: C implementation */
 #if !HAVE_CAN_INTTYPES
@@ -511,7 +511,7 @@ char *ucstrstr(char *, const char *);
 #define mkssert(e)	do { } while (/* CONSTCOND */ 0)
 #endif
 
-#if (!defined(MKSH_BUILDMAKEFILE4BSD) && !defined(MKSH_BUILDSH)) || (MKSH_BUILD_R != 421)
+#if (!defined(MKSH_BUILDMAKEFILE4BSD) && !defined(MKSH_BUILDSH)) || (MKSH_BUILD_R != 419)
 #error Must run Build.sh to compile this.
 int
 im_sorry_dave(void)
