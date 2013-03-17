f64fdd379f47b37edd10421f3dcc642514874d2f
Lucas Holt <luke@foolishgames.com>
2013-03-17 22:05:54 +0000

    drop unused function

--- lib/libmport/install.c
+++ lib/libmport/install.c
@@ -25,15 +25,12 @@
  */
 
 #include <sys/cdefs.h>
-__MBSDID("$MidnightBSD: src/lib/libmport/install.c,v 1.5 2011/07/24 15:59:08 laffer1 Exp $");
+__MBSDID("$MidnightBSD: src/lib/libmport/install.c,v 1.6 2013/03/17 21:43:55 laffer1 Exp $");
 
 #include "mport.h"
 #include "mport_private.h"
 #include <stdlib.h>
 #include <string.h>
-
-static int install_bundle_file(mportInstance *, const char *, const char *);
-static int resolve_depends(mportInstance *, mportPackageMeta *, const char *);
 
 MPORT_PUBLIC_API int mport_install(mportInstance *mport, const char *pkgname, const char *prefix)
 {
@@ -44,7 +41,7 @@ MPORT_PUBLIC_API int mport_install(mportInstance *mport, const char *pkgname, co
   MPORT_CHECK_FOR_INDEX(mport, "mport_install()");
   
   if (mport_file_exists(pkgname)) 
-    return install_bundle_file(mport, pkgname, prefix);
+    return mport_install_primative(mport, pkgname, prefix);
   
   if (mport_index_lookup_pkgname(mport, pkgname, &e) != MPORT_OK)
     RETURN_CURRENT_ERROR;
@@ -79,7 +76,6 @@ MPORT_PUBLIC_API int mport_install(mportInstance *mport, const char *pkgname, co
   if (filename == NULL) 
     RETURN_ERROR(MPORT_ERR_FATAL, "Out of memory."); 
     
-  //ret = install_bundle_file(mport, filename, prefix);
   ret = mport_install_primative(mport, filename, prefix);
 
   free(filename);
@@ -87,125 +83,4 @@ MPORT_PUBLIC_API int mport_install(mportInstance *mport, const char *pkgname, co
   mport_index_entry_free_vec(e);
   
   return ret;
-}
-
-
-static int install_bundle_file(mportInstance *mport, const char *filename, const char *prefix)
-{
-  mportBundleRead *bundle;
-  mportPackageMeta **pkgs, *pkg;
-  int i;
-  
-  if ((bundle = mport_bundle_read_new()) == NULL)
-    RETURN_ERROR(MPORT_ERR_FATAL, "Out of memory.");
-  
-  if (mport_bundle_read_init(bundle, filename) != MPORT_OK)
-    RETURN_CURRENT_ERROR;
-
-  if (mport_bundle_read_prep_for_install(mport, bundle) != MPORT_OK)
-    RETURN_CURRENT_ERROR;
-  
-  if (mport_pkgmeta_read_stub(mport, &pkgs) != MPORT_OK)
-    RETURN_CURRENT_ERROR;
-  
-  for (i=0; *(pkgs + i) != NULL; i++) {
-    pkg = pkgs[i];
-    
-    if (prefix != NULL) {
-      /* override the default prefix with the given prefix */
-      free(pkg->prefix);
-      if ((pkg->prefix = strdup(prefix)) == NULL) /* all hope is lost! bail */
-        RETURN_ERROR(MPORT_ERR_FATAL, "Out of memory.");
-    }
-
-    if (resolve_depends(mport, pkg, prefix) != MPORT_OK)
-      RETURN_CURRENT_ERROR;
-
-    if (mport_check_preconditions(mport, pkg, MPORT_PRECHECK_INSTALLED|MPORT_PRECHECK_CONFLICTS) != MPORT_OK) 
-      RETURN_CURRENT_ERROR;
-      
-    if (mport_bundle_read_install_pkg(mport, bundle, pkg) != MPORT_OK)
-      RETURN_CURRENT_ERROR;
-  }
-  
-  return MPORT_OK;
-}
-
-
-static int resolve_depends(mportInstance *mport, mportPackageMeta *pkg, const char *prefix)
-{
-  sqlite3_stmt *stmt, *lookup;
-  const char *dname, *dversion, *iversion;
-  int step, check;
-  
-  if (mport_db_prepare(mport->db, &stmt, 
-       "SELECT depend_pkgname, depend_version FROM stub.depends WHERE pkg=%Q", pkg->name) != MPORT_OK) {
-    sqlite3_finalize(stmt);
-    RETURN_CURRENT_ERROR;
-  }
-  
-  if (mport_db_prepare(mport->db, &lookup, 
-        "SELECT version FROM packages WHERE pkg=? AND status='clean'") != MPORT_OK) {
-    sqlite3_finalize(lookup);
-    RETURN_CURRENT_ERROR;
-  }
-    
-  while (1) {
-    step = sqlite3_step(stmt);
-    
-    if (step == SQLITE_ROW) {
-      dname    = sqlite3_column_text(stmt, 0); 
-      dversion = sqlite3_column_text(stmt, 1);
-
-      if (sqlite3_bind_text(lookup, 1, dname, -1, SQLITE_STATIC) != SQLITE_OK) {
-        SET_ERROR(MPORT_ERR_FATAL, sqlite3_errmsg(mport->db));
-        sqlite3_finalize(stmt); sqlite3_finalize(lookup);
-        RETURN_CURRENT_ERROR;
-      }
-
-      switch (sqlite3_step(lookup)) {
-        case SQLITE_ROW:
-          if (dversion == NULL)
-            /* no minimal version */
-            break;
-        
-          iversion = sqlite3_column_text(stmt, 0);
-          
-          check = mport_version_require_check(iversion, dversion);
-          
-          if (check == -1) {
-            /* we need to upgrade */
-           /* if (mport_upgrade(mport, dname) != MPORT_OK)
-              RETURN_CURRENT_ERROR; */
-          } else if (check > 0) {
-            RETURN_CURRENT_ERROR;
-          }
-          
-          break;
-        case SQLITE_DONE:
-          if (mport_install(mport, dname, prefix) != MPORT_OK)
-            RETURN_CURRENT_ERROR;
-            
-          break;
-        default:
-          sqlite3_finalize(lookup); 
-          sqlite3_finalize(stmt);
-          RETURN_ERROR(MPORT_ERR_FATAL, sqlite3_errmsg(mport->db));
-          break;
-      }
-      
-      sqlite3_reset(lookup);
-      sqlite3_clear_bindings(lookup);
-    } else if (step == SQLITE_DONE) {
-      break;
-    } else {
-      sqlite3_finalize(lookup); 
-      sqlite3_finalize(stmt);
-      RETURN_ERROR(MPORT_ERR_FATAL, sqlite3_errmsg(mport->db));
-    }
-  }
-  
-  sqlite3_finalize(lookup); 
-  sqlite3_finalize(stmt);
-  return MPORT_OK;           
 }
