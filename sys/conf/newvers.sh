c77d541c581afe746548ad7cab9190310d1a7624
Lucas Holt <luke@foolishgames.com>
2013-07-04 18:50:35 +0000

    prepare for 0.4-RELEASE

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -29,11 +29,11 @@
 #
 #	@(#)newvers.sh	8.1 (Berkeley) 4/20/94
 # $FreeBSD: src/sys/conf/newvers.sh,v 1.69.2.10 2006/02/01 18:04:43 kensmith Exp $
-# $MidnightBSD: src/sys/conf/newvers.sh,v 1.9 2012/10/07 14:18:18 laffer1 Exp $
+# $MidnightBSD: src/sys/conf/newvers.sh,v 1.9.2.1 2013/06/12 21:19:20 laffer1 Exp $
 
 TYPE="MidnightBSD"
 REVISION="0.4"
-BRANCH="PRERELEASE"
+BRANCH="RELEASE"
 if [ "X${BRANCH_OVERRIDE}" != "X" ]; then
 	BRANCH=${BRANCH_OVERRIDE}
 fi
