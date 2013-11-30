8611dc9ffd642e51706a7438e104237ab315614d
Lucas Holt <luke@foolishgames.com>
2013-11-30 19:49:03 +0000

    move tcsh to vendor

---
+++ patchlevel.h
@@ -0,0 +1,14 @@
+/* $Header: /home/cvs/src/contrib/tcsh/patchlevel.h,v 1.1.1.5 2012-07-08 16:12:19 laffer1 Exp $ */
+/*
+ * patchlevel.h: Our life story.
+ */
+#ifndef _h_patchlevel
+#define _h_patchlevel
+
+#define ORIGIN "Astron"
+#define REV 6
+#define VERS 18
+#define PATCHLEVEL 1
+#define DATE "2012-02-14"
+
+#endif /* _h_patchlevel */
