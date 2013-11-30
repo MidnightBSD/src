9f2dc6d1337728ab3190f215bd68449087242509
Lucas Holt <luke@foolishgames.com>
2013-11-30 19:40:57 +0000

    Move sendmail to vendor branch

---
+++ include/sm/os/sm_os_qnx.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2007 Sendmail, Inc. and its suppliers.
+ *	All rights reserved.
+ *
+ * By using this file, you agree to the terms and conditions set
+ * forth in the LICENSE file which can be found at the top level of
+ * the sendmail distribution.
+ *
+ *	$Id: sm_os_qnx.h,v 1.1.1.1 2007-11-23 22:10:30 laffer1 Exp $
+ */
+
+/*
+**  sm_os_qnx.h -- platform definitions for QNX
+*/
+
+#define SM_CONF_SYS_CDEFS_H	1
+
+#ifndef SM_CONF_SETITIMER
+# define SM_CONF_SETITIMER	0
+#endif /* SM_CONF_SETITIMER */
