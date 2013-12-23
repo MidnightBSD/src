0154c82e24af4d1d6dd6259f19fc2d417f331ea2
Lucas Holt <luke@foolishgames.com>
2013-12-23 13:14:20 +0000

    remove aout support.

--- contrib/perl/hints/midnightbsd.sh
+++ contrib/perl/hints/midnightbsd.sh
@@ -1,3 +1,4 @@
+locincpth=''
 usevfork='true'
 case "$usemymalloc" in
     "") usemymalloc='n'
@@ -5,19 +6,10 @@ case "$usemymalloc" in
 esac
 libswanted=`echo $libswanted | sed 's/ malloc / /'`
 
-objformat=`/usr/bin/objformat`
-if [ x$objformat = xaout ]; then
-    if [ -e /usr/lib/aout ]; then
-        libpth="/usr/lib/aout /usr/local/lib /usr/lib"
-        glibpth="/usr/lib/aout /usr/local/lib /usr/lib"
-    fi
-    lddlflags='-Bshareable'
-else
-    libpth="/usr/lib /usr/local/lib"
-    glibpth="/usr/lib /usr/local/lib"
-    ldflags="-Wl,-E "
-    lddlflags="-shared "
-fi
+libpth="/usr/lib /usr/local/lib"
+glibpth="/usr/lib /usr/local/lib"
+ldflags="-Wl,-E "
+lddlflags="-shared "
 cccdlflags='-DPIC -fPIC'
 
 ccflags="${ccflags} -DHAS_FPSETMASK -DHAS_FLOATINGPOINT_H"
