6eb2bdd24964a7a7f4c5c3662731f50ba4a1fcb6
Lucas Holt <luke@foolishgames.com>
2015-07-09 12:38:11 +0000

    update flex to 2.5.39

--- lib/libyywrap.c
+++
@@ -1,9 +0,0 @@
-/* libyywrap - flex run-time support library "yywrap" function */
-
-/* $Header: /home/cvs/src/usr.bin/lex/lib/libyywrap.c,v 1.2 2012-10-29 20:54:16 laffer1 Exp $
- * $MidnightBSD: trunk/usr.bin/lex/lib/libyywrap.c 6515 2013-12-20 23:32:25Z laffer1 $ */
-
-int yywrap()
-	{
-	return 1;
-	}
--- version.h
+++ version.h
@@ -1,3 +1,1 @@
-/* $MidnightBSD: trunk/usr.bin/lex/version.h 6515 2013-12-20 23:32:25Z laffer1 $ */
-
-#define FLEX_VERSION "2.5.4"
+#define FLEX_VERSION VERSION
