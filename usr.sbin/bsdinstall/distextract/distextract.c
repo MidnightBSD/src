9eaab95ba7c57de38b477455ad8087abbdfdb719
Lucas Holt <luke@foolishgames.com>
2015-08-30 16:32:15 +0000

    fix build with newer libarchive

--- usr.sbin/bsdinstall/distextract/distextract.c
+++ usr.sbin/bsdinstall/distextract/distextract.c
@@ -23,7 +23,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $MidnightBSD: src/usr.sbin/bsdinstall/distextract/distextract.c,v 1.2 2012/01/14 14:18:54 laffer1 Exp $
+ * $MidnightBSD$
  * $FreeBSD: src/usr.sbin/bsdinstall/distextract/distextract.c,v 1.4 2011/03/13 18:26:16 nwhitehorn Exp $
  */
 
@@ -127,7 +127,7 @@ count_files(const char *file)
 	/* Either we didn't have a manifest, or this archive wasn't there */
 	archive = archive_read_new();
 	archive_read_support_format_all(archive);
-	archive_read_support_compression_all(archive);
+	archive_read_support_filter_all(archive);
 	sprintf(path, "%s/%s", getenv("BSDINSTALL_DISTDIR"), file);
 	err = archive_read_open_filename(archive, path, 4096);
 	if (err != ARCHIVE_OK) {
@@ -189,7 +189,7 @@ extract_files(int nfiles, const char **files)
 	for (i = 0; i < nfiles; i++) {
 		archive = archive_read_new();
 		archive_read_support_format_all(archive);
-		archive_read_support_compression_all(archive);
+		archive_read_support_filter_all(archive);
 		sprintf(path, "%s/%s", getenv("BSDINSTALL_DISTDIR"), files[i]);
 		err = archive_read_open_filename(archive, path, 4096);
 
