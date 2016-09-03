a8da9ccee228ff8859c748fcfa839b48395470f2
Lucas Holt <luke@foolishgames.com>
2016-09-03 21:46:31 +0000

    make the min supported release 0.8 so that we have blocks

--- lib/libmport/mport_private.h
+++ lib/libmport/mport_private.h
@@ -177,10 +177,8 @@ int mport_set_errx(int , const char *, ...);
 #define MPORT_OSVERSION "0.9"
 #elif __MidnightBSD_version >= 8000
 #define MPORT_OSVERSION "0.8"
-#elif __MidnightBSD_version >= 7000
-#define MPORT_OSVERSION "0.7"
 #else
-#error "libmport only supports MidnightBSD versions 0.7 and greater."
+#error "libmport only supports MidnightBSD versions 0.8 and greater."
 #endif
 
 /* fetch stuff */
