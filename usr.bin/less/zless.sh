694d0a19020c1b30aabacc353451b3bbf15a2911
Lucas Holt <luke@foolishgames.com>
2016-09-18 22:40:31 +0000

    use two pies for zless

--- usr.bin/less/zless.sh
+++ usr.bin/less/zless.sh
@@ -2,5 +2,5 @@
 #
 # $MidnightBSD: src/usr.bin/less/zless.sh,v 1.2 2007/04/10 22:05:46 laffer1 Exp $
 
-export LESSOPEN="|/usr/bin/lesspipe.sh %s"
+export LESSOPEN="||/usr/bin/lesspipe.sh %s"
 exec /usr/bin/less "$@"
