8b9072f6075dfbb4c5ccd750cb66db12bb374734
Lucas Holt <luke@foolishgames.com>
2016-09-19 14:03:00 +0000

    fix how we handle v_writecount

--- sys/compat/linux/linux_misc.c
+++ sys/compat/linux/linux_misc.c
@@ -233,8 +233,7 @@ linux_uselib(struct thread *td, struct linux_uselib_args *args)
 	unsigned long bss_size;
 	char *library;
 	ssize_t aresid;
-	int error;
-	int locked, vfslocked;
+	int error, locked, vfslocked, writecount;
 
 	LCONVPATHEXIST(td, args->library, &library);
 
@@ -266,7 +265,10 @@ linux_uselib(struct thread *td, struct linux_uselib_args *args)
 	locked = 1;
 
 	/* Writable? */
-	if (vp->v_writecount) {
+	error = VOP_GET_WRITECOUNT(vp, &writecount);
+	if (error != 0)
+		goto cleanup;
+	if (writecount != 0) {
 		error = ETXTBSY;
 		goto cleanup;
 	}
