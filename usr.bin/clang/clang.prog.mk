a27eb571abbf2f601c2b471b2951dafc68d69dcf
Lucas Holt <luke@foolishgames.com>
2017-12-16 18:53:11 +0000

    clang 3.4

--- usr.bin/clang/clang.prog.mk
+++ usr.bin/clang/clang.prog.mk
@@ -1,4 +1,5 @@
 # $MidnightBSD$
+# $FreeBSD: stable/10/usr.bin/clang/clang.prog.mk 263508 2014-03-21 17:53:59Z dim $
 
 LLVM_SRCS= ${.CURDIR}/../../../contrib/llvm
 
@@ -8,6 +9,9 @@ LLVM_SRCS= ${.CURDIR}/../../../contrib/llvm
 DPADD+=	${.OBJDIR}/../../../lib/clang/lib${lib}/lib${lib}.a
 LDADD+=	${.OBJDIR}/../../../lib/clang/lib${lib}/lib${lib}.a
 .endfor
+
+DPADD+=	${LIBNCURSES}
+LDADD+=	-lncurses
 
 BINDIR?= /usr/bin
 
