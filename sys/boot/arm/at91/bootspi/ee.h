cc854f6b8a23a4f9719c3d0a83be92b864c0b37f
Lucas Holt <luke@foolishgames.com>
2018-05-24 11:40:01 +0000

    arm

---
+++ sys/boot/arm/at91/bootspi/ee.h
@@ -0,0 +1,7 @@
+/* $MidnightBSD$ */
+/* $FreeBSD$ */
+
+void EEInit(void);
+void EERead(unsigned ee_off, char *data_addr, unsigned size);
+void EEWrite(unsigned ee_off, const char *data_addr, unsigned size);
+
