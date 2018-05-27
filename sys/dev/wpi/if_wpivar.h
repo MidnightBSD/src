18c3c165f261b4bd034be248d93ddaee2d8362b9
Lucas Holt <luke@foolishgames.com>
2018-05-27 22:22:25 +0000

    sync with freebsd 10 stable

--- sys/dev/wpi/if_wpivar.h
+++ sys/dev/wpi/if_wpivar.h
@@ -1,4 +1,5 @@
-/*	$MidnightBSD$	*/
+/* $MidnightBSD$ */
+/*	$FreeBSD: stable/10/sys/dev/wpi/if_wpivar.h 264956 2014-04-25 22:23:38Z marius $	*/
 
 /*-
  * Copyright (c) 2006,2007
@@ -162,8 +163,6 @@ struct wpi_softc {
 	bus_space_tag_t		sc_st;
 	bus_space_handle_t	sc_sh;
 	void			*sc_ih;
-	int			mem_rid;
-	int			irq_rid;
 
 	struct wpi_config	config;
 	int			temp;
