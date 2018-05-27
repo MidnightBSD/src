fd3ef036c62287d0908aff3d5008c9b7488c4c1f
Lucas Holt <luke@foolishgames.com>
2018-05-27 16:39:45 +0000

    add dts

---
+++ sys/gnu/dts/include/dt-bindings/arm/ux500_pm_domains.h
@@ -0,0 +1,16 @@
+/* $MidnightBSD$ */
+/*
+ * Copyright (C) 2014 Linaro Ltd.
+ *
+ * Author: Ulf Hansson <ulf.hansson@linaro.org>
+ * License terms: GNU General Public License (GPL) version 2
+ */
+#ifndef _DT_BINDINGS_ARM_UX500_PM_DOMAINS_H
+#define _DT_BINDINGS_ARM_UX500_PM_DOMAINS_H
+
+#define DOMAIN_VAPE		0
+
+/* Number of PM domains. */
+#define NR_DOMAINS		(DOMAIN_VAPE + 1)
+
+#endif
