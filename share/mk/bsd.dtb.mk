a88f663c30af2bd87ea49465441de264d65dadd1
Lucas Holt <luke@foolishgames.com>
2018-06-02 21:58:00 +0000

    add bsd.dtb.mk

---
+++ share/mk/bsd.dtb.mk
@@ -0,0 +1,18 @@
+# $MidnightBSD$
+# $FreeBSD: stable/10/share/mk/bsd.dtb.mk 283930 2015-06-02 21:24:47Z imp $
+
+# Search for kernel source tree in standard places.
+.for _dir in ${.CURDIR}/../.. ${.CURDIR}/../../.. ${.CURDIR}/../../../.. \
+    ${.CURDIR}/../../../../.. /sys /usr/src/sys
+.if !defined(SYSDIR) && exists(${_dir}/kern/) && exists(${_dir}/conf/kmod.mk)
+SYSDIR=	${_dir}
+.endif
+.endfor
+.if !defined(SYSDIR) || !exists(${SYSDIR}/kern/) || \
+    !exists(${SYSDIR}/conf/kmod.mk)
+.error Unable to locate the kernel source tree. Set SYSDIR to override.
+.endif
+
+.include "${SYSDIR}/conf/dtb.mk"
+
+.include <bsd.sys.mk>
