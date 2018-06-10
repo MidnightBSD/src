62e187b3a6febfcca8e579b37f28d3a578fa983a
Lucas Holt <luke@foolishgames.com>
2018-06-10 02:45:34 +0000

    add tests

---
+++ usr.bin/xargs/tests/legacy_test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# $FreeBSD: stable/10/usr.bin/xargs/tests/legacy_test.sh 263227 2014-03-16 08:04:06Z jmmv $
+
+SRCDIR="$(dirname "${0}")"; export SRCDIR
+
+m4 "${SRCDIR}/../regress.m4" "${SRCDIR}/regress.sh" | sh
