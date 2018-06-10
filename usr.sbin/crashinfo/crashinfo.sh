7f87502f1e4cf1a0f97933414b54adf62b6a7367
Lucas Holt <luke@foolishgames.com>
2018-06-10 20:33:04 +0000

    sync

--- usr.sbin/crashinfo/crashinfo.sh
+++ usr.sbin/crashinfo/crashinfo.sh
@@ -28,11 +28,28 @@
 # SUCH DAMAGE.
 #
 # $MidnightBSD$
+# $FreeBSD: stable/10/usr.sbin/crashinfo/crashinfo.sh 303403 2016-07-27 17:55:40Z jhb $
 
 usage()
 {
 	echo "usage: crashinfo [-d crashdir] [-n dumpnr] [-k kernel] [core]"
 	exit 1
+}
+
+# Run a single gdb command against a kernel file in batch mode.
+# The kernel file is specified as the first argument and the command
+# is given in the remaining arguments.
+gdb_command()
+{
+	local k
+
+	k=$1 ; shift
+
+	if [ -x /usr/local/bin/gdb ]; then
+		/usr/local/bin/gdb -batch -ex "$@" $k
+	else
+		echo -e "$@" | /usr/bin/gdb -x /dev/stdin -batch $k
+	fi
 }
 
 find_kernel()
@@ -55,8 +72,8 @@ find_kernel()
 
 	# Look for a matching kernel version.
 	for k in `sysctl -n kern.bootfile` $(ls -t /boot/*/kernel); do
-		kvers=$(echo 'printf "  Version String: %s", version' | \
-		    gdb -x /dev/stdin -batch $k 2>/dev/null)
+		kvers=$(gdb_command $k 'printf "  Version String: %s", version' \
+		     2>/dev/null)
 		if [ "$ivers" = "$kvers" ]; then
 			KERNEL=$k
 			break
@@ -151,11 +168,10 @@ echo "Writing crash summary to $FILE."
 umask 077
 
 # Simulate uname
-ostype=$(echo -e printf '"%s", ostype' | gdb -x /dev/stdin -batch $KERNEL)
-osrelease=$(echo -e printf '"%s", osrelease' | gdb -x /dev/stdin -batch $KERNEL)
-version=$(echo -e printf '"%s", version' | gdb -x /dev/stdin -batch $KERNEL | \
-    tr '\t\n' '  ')
-machine=$(echo -e printf '"%s", machine' | gdb -x /dev/stdin -batch $KERNEL)
+ostype=$(gdb_command $KERNEL 'printf "%s", ostype')
+osrelease=$(gdb_command $KERNEL 'printf "%s", osrelease')
+version=$(gdb_command $KERNEL 'printf "%s", version' | tr '\t\n' '  ')
+machine=$(gdb_command $KERNEL 'printf "%s", machine')
 
 exec > $FILE 2>&1
 
@@ -174,16 +190,20 @@ file=`mktemp /tmp/crashinfo.XXXXXX`
 if [ $? -eq 0 ]; then
 	echo "bt" >> $file
 	echo "quit" >> $file
-	kgdb $KERNEL $VMCORE < $file
+	if [ -x /usr/local/bin/kgdb ]; then
+		/usr/local/bin/kgdb $KERNEL $VMCORE < $file
+	else
+		kgdb $KERNEL $VMCORE < $file
+	fi
 	rm -f $file
 	echo
 fi
 echo
 
 echo "------------------------------------------------------------------------"
-echo "ps -axl"
+echo "ps -axlww"
 echo
-ps -M $VMCORE -N $KERNEL -axl
+ps -M $VMCORE -N $KERNEL -axlww
 echo
 
 echo "------------------------------------------------------------------------"
@@ -265,12 +285,6 @@ echo "------------------------------------------------------------------------"
 echo "netstat -m"
 echo
 netstat -M $VMCORE -N $KERNEL -m
-echo
-
-echo "------------------------------------------------------------------------"
-echo "netstat -id"
-echo
-netstat -M $VMCORE -N $KERNEL -id
 echo
 
 echo "------------------------------------------------------------------------"
