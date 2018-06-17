8130078e7f9bd3dcf56431547fca5337c61c4111
Lucas Holt <luke@foolishgames.com>
2018-06-17 19:16:12 +0000

    tag

--- kerberos5/include/version.h
+++ kerberos5/include/version.h
@@ -1,6 +1,7 @@
 /* $MidnightBSD$ */
+/* $FreeBSD: stable/10/kerberos5/include/version.h 234027 2012-04-08 08:19:17Z stas $ */
 #ifndef VERSION_HIDDEN
 #define VERSION_HIDDEN
 #endif
-VERSION_HIDDEN const char *heimdal_long_version = "@(#)$Version: Heimdal 1.5.2 (MidnightBSD) $";
+VERSION_HIDDEN const char *heimdal_long_version = "@(#)$Version: Heimdal 1.5.2 (FreeBSD) $";
 VERSION_HIDDEN const char *heimdal_version = "Heimdal 1.5.2";
