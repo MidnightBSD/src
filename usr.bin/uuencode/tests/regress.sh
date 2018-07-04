1ea5a0b7615cc6a529c5262a461b45e8b3d85718
Lucas Holt <luke@foolishgames.com>
2018-07-04 20:50:29 +0000

    add more test

---
+++ usr.bin/uuencode/tests/regress.sh
@@ -0,0 +1,11 @@
+# $MidnightBSD$
+# $FreeBSD: stable/10/usr.bin/uuencode/tests/regress.sh 263227 2014-03-16 08:04:06Z jmmv $
+
+echo 1..2
+
+REGRESSION_START($1)
+
+REGRESSION_TEST(`traditional', `uuencode regress.in <${SRCDIR}/regress.in')
+REGRESSION_TEST(`base64', `uuencode -m regress.in <${SRCDIR}/regress.in')
+
+REGRESSION_END()
