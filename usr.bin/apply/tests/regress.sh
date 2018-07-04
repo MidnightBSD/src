de2e560bc373e542dedd85901d6af6f7605165d4
Lucas Holt <luke@foolishgames.com>
2018-07-04 00:04:12 +0000

    add tests

---
+++ usr.bin/apply/tests/regress.sh
@@ -0,0 +1,10 @@
+# $FreeBSD: stable/10/usr.bin/apply/tests/regress.sh 263227 2014-03-16 08:04:06Z jmmv $
+
+echo 1..2
+
+REGRESSION_START($1)
+
+REGRESSION_TEST(`00', `apply "echo %1 %1 %1 %1" $(cat ${SRCDIR}/regress.00.in)')
+REGRESSION_TEST(`01', `sh ${SRCDIR}/regress.01.sh')
+
+REGRESSION_END()
