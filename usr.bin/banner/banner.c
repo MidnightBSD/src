d2b0e7f55ee1cf0670965f26562fb03fe73df2ff
Lucas Holt <luke@foolishgames.com>
2018-07-07 15:53:47 +0000

    make things static

--- usr.bin/banner/banner.c
+++ usr.bin/banner/banner.c
@@ -1,3 +1,4 @@
+/* $MidnightBSD$ */
 /*
  * Copyright (c) 1980, 1993, 1994
  *	The Regents of the University of California.  All rights reserved.
@@ -40,7 +41,7 @@ static char sccsid[] = "@(#)banner.c	8.4 (Berkeley) 4/29/95";
 #endif
 
 #include <sys/cdefs.h>
-__MBSDID("$MidnightBSD$");
+__FBSDID("$FreeBSD: stable/10/usr.bin/banner/banner.c 319256 2017-05-30 21:58:53Z asomers $");
 
 /*
  * banner - prints large signs
@@ -59,7 +60,7 @@ __MBSDID("$MidnightBSD$");
 #define NBYTES 9271
 
 /* Pointers into data_table for each ASCII char */
-const int asc_ptr[NCHARS] = {
+static const int asc_ptr[NCHARS] = {
 /* ^@ */   0,      0,      0,      0,      0,      0,      0,      0,
 /* ^H */   0,      0,      0,      0,      0,      0,      0,      0,
 /* ^P */   0,      0,      0,      0,      0,      0,      0,      0,
@@ -86,7 +87,7 @@ const int asc_ptr[NCHARS] = {
  * is the next elt in array) and goto second
  * next element in array.
  */
-const unsigned char data_table[NBYTES] = {
+static const unsigned char data_table[NBYTES] = {
 /*             0     1     2     3     4     5     6     7     8     9 */
 /*    0 */   129,  227,  130,   34,    6,   90,   19,  129,   32,   10,
 /*   10 */    74,   40,  129,   31,   12,   64,   53,  129,   30,   14,
@@ -1018,11 +1019,11 @@ const unsigned char data_table[NBYTES] = {
 /* 9270 */   193
 };
 
-char	line[DWIDTH];
-char	*message;
-char	print[DWIDTH];
-int	debug, i, j, linen, max, nchars, pc, term, trace, x, y;
-int	width = DWIDTH;	/* -w option: scrunch letters to 80 columns */
+static char	line[DWIDTH];
+static char	*message;
+static char	print[DWIDTH];
+static int	debug, i, j, linen, max, nchars, pc, term, trace, x, y;
+static int	width = DWIDTH;	/* -w option: scrunch letters to 80 columns */
 
 static void usage(void);
 
@@ -1064,8 +1065,8 @@ main(int argc, char *argv[])
 			err(1, "malloc");
 		strcpy(message, *argv);
 		while (*++argv) {
-			strcat(message, " ");
-			strcat(message, *argv);
+			strlcat(message, " ", j);
+			strlcat(message, *argv, j);
 		}
 		nchars = strlen(message);
 	} else {
