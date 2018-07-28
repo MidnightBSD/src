549b2a8f30b75bde42296b6172d6820a13e5f2e9
Lucas Holt <luke@foolishgames.com>
2018-07-28 20:40:46 +0000

    add tests

---
+++ tests/sys/pjdfstest/tests/pjdfstest.test.mk
@@ -0,0 +1,15 @@
+# $MidnightBSD$
+# $FreeBSD: stable/10/tests/sys/pjdfstest/tests/pjdfstest.test.mk 274016 2014-11-03 07:18:42Z ngie $
+
+PJDFSTEST_SRCDIR=	${.CURDIR:H:H:H:H:H}/contrib/pjdfstest
+
+.PATH: ${PJDFSTEST_SRCDIR}/tests/${.CURDIR:T}
+
+TESTSDIR?=	${TESTSBASE}/sys/pjdfstest/${.CURDIR:T}
+
+.for s in ${TAP_TESTS_SH}
+TAP_TESTS_SH_SRC_$s=	$s.t
+TEST_METADATA.$s+=	required_user="root"
+.endfor
+
+.include <bsd.test.mk>
