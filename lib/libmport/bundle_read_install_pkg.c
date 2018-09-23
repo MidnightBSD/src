3347aacdd1467144b6f4caa2d23922c8fc8d2e38
Lucas Holt <luke@foolishgames.com>
2018-09-23 15:33:32 +0000

    use proper format

--- lib/libmport/bundle_read_install_pkg.c
+++ lib/libmport/bundle_read_install_pkg.c
@@ -179,7 +179,7 @@ create_package_row(mportInstance *mport, mportPackageMeta *pkg)
 	/* Insert the package meta row into the packages table (We use pack here because things might have been twiddled) */
 	/* Note that this will be marked as dirty by default */
 	if (mport_db_do(mport->db,
-	                "INSERT INTO packages (pkg, version, origin, prefix, lang, options, comment, os_release, cpe, locked, deprecated, expiration_date, no_provide_shlib, flavor) VALUES (%Q,%Q,%Q,%Q,%Q,%Q,%Q,%Q,%Q,0,%Q,%Q,%Q,%Q)",
+	                "INSERT INTO packages (pkg, version, origin, prefix, lang, options, comment, os_release, cpe, locked, deprecated, expiration_date, no_provide_shlib, flavor) VALUES (%Q,%Q,%Q,%Q,%Q,%Q,%Q,%Q,%Q,0,%Q,%ld,%d,%Q)",
 	                pkg->name, pkg->version, pkg->origin, pkg->prefix, pkg->lang, pkg->options, pkg->comment,
 	                pkg->os_release, pkg->cpe, pkg->deprecated, pkg->expiration_date, pkg->no_provide_shlib, pkg->flavor) != MPORT_OK)
 		RETURN_CURRENT_ERROR;
