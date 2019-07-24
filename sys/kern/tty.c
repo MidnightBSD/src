48c3bb81fa2f04018d44a753b13534756fa2585d
Lucas Holt <luke@foolishgames.com>
2019-07-24 22:51:58 +0000

    The code which handles a close(2) of a descriptor created by posix_openpt(2) fails to undo the configuration which causes SIGIO to be raised.  This bug can lead to a write-after-free of kernel memory.

--- sys/kern/tty.c
+++ sys/kern/tty.c
@@ -202,9 +202,6 @@ ttydev_leave(struct tty *tp)
 
 	tp->t_flags |= TF_OPENCLOSE;
 
-	/* Stop asynchronous I/O. */
-	funsetown(&tp->t_sigio);
-
 	/* Remove console TTY. */
 	if (constty == tp)
 		constty_clear();
@@ -1092,6 +1089,9 @@ tty_rel_free(struct tty *tp)
 		tty_unlock(tp);
 		return;
 	}
+
+	/* Stop asynchronous I/O. */
+	funsetown(&tp->t_sigio);
 
 	/* TTY can be deallocated. */
 	dev = tp->t_dev;
