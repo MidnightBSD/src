17af240c4db5f5b3c133dd5958ef8ed33efd9d92
Lucas Holt <luke@foolishgames.com>
2020-05-05 00:12:32 -0400

    cleanup make files for clang

--- lib/clang/lldb.lib.mk
+++
@@ -1,9 +0,0 @@
-# $MidnightBSD$
-# $FreeBSD: stable/10/lib/clang/lldb.lib.mk 255722 2013-09-20 01:52:02Z emaste $
-
-LLDB_SRCS= ${.CURDIR}/../../../contrib/llvm/tools/lldb
-
-CFLAGS+=-I${LLDB_SRCS}/include -I${LLDB_SRCS}/source
-CXXFLAGS+=-std=c++11 -DLLDB_DISABLE_PYTHON                      
-
-.include "clang.lib.mk"
