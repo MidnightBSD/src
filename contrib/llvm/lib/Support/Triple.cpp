8a860b017a37a620d9b458575a172ab080a70755
Lucas Holt <luke@foolishgames.com>
2020-09-08 15:55:03 -0400

    bump ver

--- contrib/llvm/lib/Support/Triple.cpp
+++ contrib/llvm/lib/Support/Triple.cpp
@@ -477,6 +477,7 @@ static Triple::OSType parseOS(StringRef OSName) {
     .StartsWith("darwin", Triple::Darwin)
     .StartsWith("dragonfly", Triple::DragonFly)
     .StartsWith("freebsd", Triple::FreeBSD)
+    .StartsWith("midnightbsd", Triple::FreeBSD)
     .StartsWith("fuchsia", Triple::Fuchsia)
     .StartsWith("ios", Triple::IOS)
     .StartsWith("kfreebsd", Triple::KFreeBSD)
