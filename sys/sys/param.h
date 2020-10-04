6046cfec2180e2efd7acd182af7852fc7d1962af
Lucas Holt <luke@foolishgames.com>
2020-10-04 15:44:35 -0400

    bump for perl and mdnsresponder imports

--- sys/sys/param.h
+++ sys/sys/param.h
@@ -51,7 +51,7 @@
  * 		or r is 5-9 in stable after 1.0 is released.
  */
 #undef __MidnightBSD_version
-#define __MidnightBSD_version 200001	/* Master, propagated to newvers */
+#define __MidnightBSD_version 200002	/* Master, propagated to newvers */
 
 /* Version of FreeBSD we're compatible with */
 #undef __FreeBSD_version
