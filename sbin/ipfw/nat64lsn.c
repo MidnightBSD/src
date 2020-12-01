689d6d00090f8c0052d99a9cdba4a6c1aeb2e777
Lucas Holt <luke@foolishgames.com>
2020-12-01 16:03:25 -0500

    initialize vars in ipfw

--- sbin/ipfw/nat64lsn.c
+++ sbin/ipfw/nat64lsn.c
@@ -97,6 +97,7 @@ nat64lsn_print_states(void *buf)
 	stg = (ipfw_nat64lsn_stg *)(od + 1);
 	sz = od->head.length - sizeof(*od);
 	next_idx = 0;
+	proto = NULL;
 	while (sz > 0 && next_idx != 0xFF) {
 		next_idx = stg->next_idx;
 		sz -= sizeof(*stg);
