054a04cb89846b617984b23f60b4525dbf7ca8e7
Lucas Holt <luke@foolishgames.com>
2020-12-15 13:55:30 -0500

    updatee patch

--- crypto/openssl/crypto/x509v3/v3_genn.c
+++ crypto/openssl/crypto/x509v3/v3_genn.c
@@ -139,37 +139,6 @@ static int edipartyname_cmp(const EDIPARTYNAME *a, const EDIPARTYNAME *b)
     return ASN1_STRING_cmp(a->partyName, b->partyName);
 }
 
-static int edipartyname_cmp(const EDIPARTYNAME *a, const EDIPARTYNAME *b)
-{
-    int res;
-
-    if (a == NULL || b == NULL) {
-        /*
-         * Shouldn't be possible in a valid GENERAL_NAME, but we handle it
-         * anyway. OTHERNAME_cmp treats NULL != NULL so we do the same here
-         */
-        return -1;
-    }
-    if (a->nameAssigner == NULL && b->nameAssigner != NULL)
-        return -1;
-    if (a->nameAssigner != NULL && b->nameAssigner == NULL)
-        return 1;
-    /* If we get here then both have nameAssigner set, or both unset */
-    if (a->nameAssigner != NULL) {
-        res = ASN1_STRING_cmp(a->nameAssigner, b->nameAssigner);
-        if (res != 0)
-            return res;
-    }
-    /*
-     * partyName is required, so these should never be NULL. We treat it in
-     * the same way as the a == NULL || b == NULL case above
-     */
-    if (a->partyName == NULL || b->partyName == NULL)
-        return -1;
-
-    return ASN1_STRING_cmp(a->partyName, b->partyName);
-}
-
 /* Returns 0 if they are equal, != 0 otherwise. */
 int GENERAL_NAME_cmp(GENERAL_NAME *a, GENERAL_NAME *b)
 {
