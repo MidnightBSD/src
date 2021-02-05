5d4bc6b753f30d0a55e05d7c6d9f04d09926ea65
Lucas Holt <luke@foolishgames.com>
2021-02-05 10:37:58 -0500

    add ifdef

--- contrib/libc-vis/vis.c
+++ contrib/libc-vis/vis.c
@@ -142,7 +142,7 @@ static const wchar_t char_glob[] = L"*?[#";
  * loops below are using sizeof(uint64_t) - 1 instead of MB_LEN_MAX, and
  * the assertion is commented out.
  */
-#ifdef __FreeBSD__
+#if defined(__FreeBSD__) || defined(__MidnightBSD__)
 /*
  * On FreeBSD including <sys/systm.h> for CTASSERT only works in kernel
  * mode.
