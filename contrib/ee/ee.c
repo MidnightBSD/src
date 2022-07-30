ec987109acc0d22fe10925ef49122f2ee1957300
Lucas Holt <luke@foolishgames.com>
2022-07-30 16:33:20 -0400

    cleanup

--- contrib/ee/ee.c
+++ contrib/ee/ee.c
@@ -55,7 +55,7 @@
  */
 
 #include <sys/cdefs.h>
-__FBSDID("$FreeBSD: stable/11/contrib/ee/ee.c 245952 2013-01-26 22:08:21Z pfg $");
+__FBSDID("$FreeBSD$");
 
 char *ee_copyright_message = 
 "Copyright (c) 1986, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 2009 Hugh Mahon ";
