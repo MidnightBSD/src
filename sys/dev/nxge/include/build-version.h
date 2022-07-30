d508c070f3a429fb474fa9d19f20b6c416bf315c
Lucas Holt <luke@foolishgames.com>
2022-07-30 15:05:15 -0400

    remove old driver code.

--- sys/dev/nxge/include/build-version.h
+++
@@ -1,6 +0,0 @@
-#ifndef BUILD_VERSION_H
-#define BUILD_VERSION_H
-/* Do not edit! Automatically generated when released.*/
-/* $FreeBSD: stable/11/sys/dev/nxge/include/build-version.h 173139 2007-10-29 14:19:32Z rwatson $ */
-#define GENERATED_BUILD_VERSION "11230"
-#endif /* BUILD_VERSION_H */
--- sys/dev/vxge/include/build-version.h
+++
@@ -1,7 +0,0 @@
-/* $FreeBSD: stable/11/sys/dev/vxge/include/build-version.h 221389 2011-05-03 15:58:24Z gnn $ */
-
-#ifndef	BUILD_VERSION_H
-#define	BUILD_VERSION_H
-/* Do not edit! Automatically generated when released. */
-#define	GENERATED_BUILD_VERSION 22770
-#endif /* BUILD_VERSION_H */
