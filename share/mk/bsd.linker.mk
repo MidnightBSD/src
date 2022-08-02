15099b56065acff0b3bc46ed5be00708fe8c539d
Lucas Holt <luke@foolishgames.com>
2022-08-02 11:06:50 -0400

    midnight not free

--- share/mk/bsd.linker.mk
+++ share/mk/bsd.linker.mk
@@ -1,5 +1,3 @@
-# $FreeBSD$
-
 # Setup variables for the linker.
 #
 # LINKER_TYPE is the major type of linker. Currently binutils and lld support
@@ -16,7 +14,7 @@
 # - retpoline: support for generating PLT with retpoline speculative
 #              execution vulnerability mitigation
 #
-# LINKER_FREEBSD_VERSION is the linker's internal source version.
+# LINKER_MIDNIGHTBSD_VERSION is the linker's internal source version.
 #
 # These variables with an X_ prefix will also be provided if XLD is set.
 #
@@ -32,7 +30,7 @@ __<bsd.linker.mk>__:
 # The value is only used/exported for the same environment that impacts
 # LD and LINKER_* settings here.
 _exported_vars=	${X_}LINKER_TYPE ${X_}LINKER_VERSION ${X_}LINKER_FEATURES \
-		${X_}LINKER_FREEBSD_VERSION
+		${X_}LINKER_MIDNIGHTBSD_VERSION
 ${X_}_ld_hash=	${${ld}}${MACHINE}${PATH}
 ${X_}_ld_hash:=	${${X_}_ld_hash:hash}
 # Only import if none of the vars are set somehow else.
@@ -58,15 +56,15 @@ _ld_version!=	(${${ld}} --version || echo none) | sed -n 1p
 .endif
 .if ${_ld_version:[1..2]} == "GNU ld"
 ${X_}LINKER_TYPE=	bfd
-${X_}LINKER_FREEBSD_VERSION=	0
+${X_}LINKER_MIDNIGHTBSD_VERSION=	0
 _v=	${_ld_version:M[1-9]*.[0-9]*:[1]}
 .elif ${_ld_version:[1]} == "LLD"
 ${X_}LINKER_TYPE=	lld
 _v=	${_ld_version:[2]}
 .if ${_ld_version:[3]} == "(FreeBSD"
-${X_}LINKER_FREEBSD_VERSION:=	${_ld_version:[4]:C/.*-([^-]*)\)/\1/}
+${X_}LINKER_MIDNIGHTBSD_VERSION:=	${_ld_version:[4]:C/.*-([^-]*)\)/\1/}
 .else
-${X_}LINKER_FREEBSD_VERSION=	0
+${X_}LINKER_MIDNIGHTBSD_VERSION=	0
 .endif
 .else
 .warning Unknown linker from ${ld}=${${ld}}: ${_ld_version}, defaulting to bfd
@@ -97,7 +95,7 @@ ${X_}LINKER_FEATURES+=	retpoline
 X_LINKER_TYPE=		${LINKER_TYPE}
 X_LINKER_VERSION=	${LINKER_VERSION}
 X_LINKER_FEATURES=	${LINKER_FEATURES}
-X_LINKER_FREEBSD_VERSION= ${LINKER_FREEBSD_VERSION}
+X_LINKER_MIDNIGHTBSD_VERSION= ${LINKER_MIDNIGHTBSD_VERSION}
 .endif	# ${ld} == "LD" || (${ld} == "XLD" && ${XLD} != ${LD})
 
 # Export the values so sub-makes don't have to look them up again, using the
