d4cd1bc7ed5b50479a6d98791973fa3e99b3b9c7
Dimitry Andric <dim@FreeBSD.org>
2022-08-15 20:46:44 +0200

    Adjust function definitions in mp_cpudep.c.c to avoid clang 15 warnings
    With clang 15, the following -Werror warnings are produced:
    
        sys/powerpc/booke/mp_cpudep.c:54:20: error: a function declaration without a prototype is deprecated in all versions of C [-Werror,-Wstrict-prototypes]
        cpudep_ap_bootstrap()
                           ^
                            void
        sys/powerpc/booke/mp_cpudep.c:97:16: error: a function declaration without a prototype is deprecated in all versions of C [-Werror,-Wstrict-prototypes]
        cpudep_ap_setup()
                       ^
                        void
    
    This is because cpudep_ap_bootstrap() and cpudep_ap_setup() are declared
    with (void) argument lists, but defined with empty argument lists. Make
    the definitions match the declarations.
    
    MFC after:	3 days
    
    (cherry picked from commit 02dd51c9bccedc2fc8d98c3892986232f6bacdab)

--- sys/powerpc/booke/mp_cpudep.c
+++ sys/powerpc/booke/mp_cpudep.c
@@ -49,7 +49,7 @@ extern void icache_inval(void);
 volatile void *ap_pcpu;
 
 uintptr_t
-cpudep_ap_bootstrap()
+cpudep_ap_bootstrap(void)
 {
 	uint32_t msr, csr;
 	uintptr_t sp;
@@ -91,6 +91,6 @@ cpudep_ap_bootstrap()
 }
 
 void
-cpudep_ap_setup()
+cpudep_ap_setup(void)
 {
 }
