14a4d0bf8d74892daf9cc64ac68c97f8d8490acd
Lucas Holt <luke@foolishgames.com>
2022-08-20 15:42:55 -0400

    fix miss

--- usr.bin/factor/factor.c
+++ usr.bin/factor/factor.c
@@ -42,7 +42,6 @@ __SCCSID("@(#)factor.c	8.4 (Berkeley) 5/4/95");
 #ifdef __RCSID
 __RCSID("$NetBSD: factor.c,v 1.19 2009/08/12 05:54:31 dholland Exp $");
 #endif
-#endif
 #endif /* not lint */
 
 /*
