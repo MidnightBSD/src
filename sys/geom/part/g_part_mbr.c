6bd539c21e2558c535bfded3626f31ec81a5328c
Lucas Holt <luke@foolishgames.com>
2022-10-20 13:32:58 -0400

    remove dead pc98 code and fix x86 mbr partition type

--- sys/geom/part/g_part_mbr.c
+++ sys/geom/part/g_part_mbr.c
@@ -126,7 +126,7 @@ static struct g_part_mbr_alias {
 	u_char		typ;
 	int		alias;
 } mbr_alias_match[] = {
-	{ DOSPTYP_386BSD,	G_PART_ALIAS_FREEBSD },
+	{ DOSPTYP_386BSD,	G_PART_ALIAS_MIDNIGHTBSD },
 	{ DOSPTYP_APPLE_BOOT,	G_PART_ALIAS_APPLE_BOOT },
 	{ DOSPTYP_APPLE_UFS,	G_PART_ALIAS_APPLE_UFS },
 	{ DOSPTYP_EFI,		G_PART_ALIAS_EFI },
