89bdc71bf695cbaa403bf60970566079cc873e2b
Lucas Holt <luke@foolishgames.com>
2022-11-21 16:19:33 -0500

    cleanup gnu directory

--- gnu/usr.bin/binutils/as/ia64-freebsd/targ-cpu.h
+++
@@ -1,4 +0,0 @@
-/* $MidnightBSD$ */
-/* $FreeBSD: stable/10/gnu/usr.bin/binutils/as/ia64-freebsd/targ-cpu.h 84954 2001-10-15 02:13:26Z obrien $ */
-
-#include "tc-ia64.h"
--- gnu/usr.bin/binutils/as/targ-env.h
+++ gnu/usr.bin/binutils/as/targ-env.h
@@ -1,3 +1,2 @@
-/* $FreeBSD: stable/11/gnu/usr.bin/binutils/as/targ-env.h 84949 2001-10-15 01:43:23Z obrien $ */
 
 #include "te-freebsd.h"
