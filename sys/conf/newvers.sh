34b0548b03267652d3647e0702057ae2ea3c9f6b
Lucas Holt <luke@foolishgames.com>
2023-03-29 14:34:28 -0400

    Bump OS version to 3.0.1

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -31,7 +31,7 @@
 # $FreeBSD: stable/11/sys/conf/newvers.sh 349884 2019-07-10 13:35:01Z gjb $
 
 TYPE="MidnightBSD"
-REVISION="3.0.0"
+REVISION="3.0.1"
 RELEASE="${REVISION}"
 VERSION="${TYPE} ${RELEASE}"
 
