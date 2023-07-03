1c75ea1798f112fdf63d74988e6b81f6d9a9a710
Lucas Holt <luke@foolishgames.com>
2023-07-02 23:53:43 -0400

    drop $FreeBSD$

--- usr.sbin/config/main.c
+++ usr.sbin/config/main.c
@@ -39,7 +39,6 @@ static const char copyright[] =
 #if 0
 static char sccsid[] = "@(#)main.c	8.1 (Berkeley) 6/6/93";
 #endif
-static const char rcsid[] =
 #endif /* not lint */
 
 #include <sys/types.h>
