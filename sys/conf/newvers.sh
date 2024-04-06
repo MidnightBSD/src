bf6992d06bc1e4eb9827bb33d7d5daa4695c7ac3
Lucas Holt <luke@foolishgames.com>
2024-04-06 15:33:03 -0400

    bump after release.

--- sys/conf/newvers.sh
+++ sys/conf/newvers.sh
@@ -45,7 +45,7 @@
 # upper case variables starting in column 1 are on one line w/o continuation.
 
 TYPE="MidnightBSD"
-REVISION="3.1.4"
+REVISION="3.1.5"
 RELEASE="${REVISION}"
 VERSION="${TYPE} ${RELEASE}"
 
