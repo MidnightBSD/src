5afbf405e2d03476dc215baced19c8081b046c13
Lucas Holt <luke@foolishgames.com>
2024-11-12 00:26:16 -0500

    cleanup

--- gnu/usr.bin/binutils/ld/armelf_fbsd.sh
+++
@@ -1,9 +0,0 @@
-# $FreeBSD: stable/11/gnu/usr.bin/binutils/ld/armelf_fbsd.sh 245101 2013-01-06 07:14:04Z andrew $
-. ${srcdir}/emulparams/armelf.sh
-. ${srcdir}/emulparams/elf_fbsd.sh
-TARGET2_TYPE=got-rel
-MAXPAGESIZE=0x8000
-GENERATE_PIE_SCRIPT=yes
-
-unset STACK_ADDR
-unset EMBEDDED
