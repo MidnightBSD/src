markjdb/freebsd

qlnxe: Fix setting the unicast MAC filter of RX path
When an Ethernet interface is added to lagg(4) as a child interface, its
type, aka if_type, is changed from IFT_ETHER to IFT_IEEE8023ADLAG. Well
changing the link-layer address of the lagg(4) interface will be
propagated to all child interfaces, hence the drivers of child interfaces
shall not presume the type of the interface will not be changed.

Meanwhile, on initializing, an ifnet has been fully attached and it is
guaranteed to have non-null link-layer address so stop NULL checking for
it.

Reviewed by:	kbowling
Fixes:		792226e53023 qlnxe: Allow MAC address override
MFC after:	5 days
Differential Revision:	https://reviews.freebsd.org/D54885

sys/dev/qlnx/qlnxe/qlnx_os.c
@@ -7057,20 +7057,10 @@ qlnx_set_rx_mode(qlnx_host_t *ha)
 	int	rc = 0;
 	uint8_t	filter;
 	const if_t ifp = ha->ifp;
-	const struct ifaddr *ifa;
-	struct sockaddr_dl *sdl;
 
-	ifa = if_getifaddr(ifp);
-	if (if_gettype(ifp) == IFT_ETHER && ifa != NULL &&
-			ifa->ifa_addr != NULL) {
-		sdl = (struct sockaddr_dl *) ifa->ifa_addr;
-
-		rc = qlnx_set_ucast_rx_mac(ha, ECORE_FILTER_REPLACE, LLADDR(sdl));
-	} else {
-		rc = qlnx_set_ucast_rx_mac(ha, ECORE_FILTER_REPLACE, ha->primary_mac);
-	}
-        if (rc)
-                return rc;
+	rc = qlnx_set_ucast_rx_mac(ha, ECORE_FILTER_REPLACE, if_getlladdr(ifp));
+	if (rc)
+		return rc;
 
 	rc = qlnx_remove_all_mcast_mac(ha);
         if (rc)

MidnightBSD/src

(cherry picked from commit f250852c9a0c1021c3be4b498e27cfc7b42a81db)
(cherry picked from commit 6d138e958ffb318595eec29b910cada414e2f86d)
(cherry picked from commit 93719f8c8348a5d13c9037352072ce67f530288b)

sys/dev/qlnx/qlnxe/qlnx_os.c
@@ -7185,18 +7185,10 @@ qlnx_set_rx_mode(qlnx_host_t *ha)
 	int	rc = 0;
 	uint8_t	filter;
 	const struct ifnet *ifp = ha->ifp;
-	struct sockaddr_dl *sdl;
 
-	if (ifp->if_type == IFT_ETHER && ifp->if_addr != NULL &&
-			ifp->if_addr->ifa_addr != NULL) {
-		sdl = (struct sockaddr_dl *) ifp->if_addr->ifa_addr;
-
-		rc = qlnx_set_ucast_rx_mac(ha, ECORE_FILTER_REPLACE, LLADDR(sdl));
-	} else {
-		rc = qlnx_set_ucast_rx_mac(ha, ECORE_FILTER_REPLACE, ha->primary_mac);
-	}
-        if (rc)
-                return rc;
+	rc = qlnx_set_ucast_rx_mac(ha, ECORE_FILTER_REPLACE, IF_LLADDR(ifp));
+	if (rc)
+		return rc;
 
 	rc = qlnx_remove_all_mcast_mac(ha);
         if (rc)